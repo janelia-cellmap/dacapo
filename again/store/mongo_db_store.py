cefbfb7e4ed0f0371cdf362f5780732fc0889341
Jan Funke <funkej@janelia.hhmi.org>
2020-01-31 17:57:09 -0500

    Use upsert to store configs in MongoDB

--- again/store/mongo_db_store.py
+++ again/store/mongo_db_store.py
@@ -247,19 +247,21 @@ class MongoDbStore:
 
     def __save_insert(self, collection, data, ignore=None):
 
-        existing = list(
-            collection.find(
-                {'id': data['id']}, {'_id': False}))
+        existing = collection.find_one_and_update(
+                filter={'id': data['id']},
+                update={'$set': data},
+                upsert=True,
+                projection={'_id': False})
 
-        if len(existing) > 0:
+        if existing:
 
-            if not self.__same_doc(existing[0], data, ignore):
+            if not self.__same_doc(existing, data, ignore):
                 raise RuntimeError(
                     f"Data for {data['id']} does not match already synced "
-                    f"entry. Found\n\n{existing[0]}\n\nin DB, but was "
+                    f"entry. Found\n\n{existing}\n\nin DB, but was "
                     f"given\n\n{data}")
-        else:
-            collection.insert(data)
+
+        return existing
 
     def __same_doc(self, a, b, ignore=None):
 
