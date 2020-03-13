fb0b66cfbe39adebe137c5ef1c978a3852139500
Jan Funke <funkej@janelia.hhmi.org>
2020-03-13 17:38:50 -0400

    Add option to select score in validation plot

--- again/analyze/plot.py
+++ again/analyze/plot.py
@@ -28,7 +28,7 @@ def smooth_values(a, n, stride=1):
 
     return m, s
 
-def plot_runs(runs, smooth=100):
+def plot_runs(runs, smooth=100, validation_score=None):
 
     run_names = [str(run) for run in runs]
     max_iteration = max([run.training_stats.trained_until() for run in runs])
@@ -49,14 +49,20 @@ def plot_runs(runs, smooth=100):
     loss_figure.background_fill_color = '#efefef'
 
 
+    validation_score_names = []
+    for r in runs:
+        if r.validation_scores.validated_until() > 0:
+            validation_score_names += r.validation_scores.get_score_names()
+    validation_score_names = np.unique(validation_score_names)
+
     validation_tooltips = [
         ("task", "@task"),
         ("model", "@model"),
         ("optimizer", "@optimizer"),
-        ("iteration", "@iteration"),
-        ("voi_split", "@voi_split"),
-        ("voi_merge", "@voi_merge"),
-        ("voi_sum", "@voi_sum")
+        ("iteration", "@iteration")
+    ] + [
+        (name, '@' + name)
+        for name in validation_score_names
     ]
     validation_figure = bokeh.plotting.figure(
         tools="pan, wheel_zoom, reset, save, hover",
@@ -119,47 +125,28 @@ def plot_runs(runs, smooth=100):
                 color=color,
                 alpha=0.3)
 
-        if run.validation_scores.validated_until() > 0:
+        if validation_score and run.validation_scores.validated_until() > 0:
+
+            x = run.validation_scores.iterations
+            source_dict = {
+                'iteration': x,
+                'task': [run.task_config.id]*len(x),
+                'model': [run.model_config.id]*len(x),
+                'optimizer': [run.optimizer_config.id]*len(x)
+            }
             validation_averages = run.validation_scores.get_averages()
+            source_dict.update({
+                name: np.array(validation_averages[name])
+                for name in run.validation_scores.get_score_names()
+            })
+            source = bokeh.plotting.ColumnDataSource(source_dict)
+            validation_figure.line(
+                'iteration', validation_score,
+                legend_label=name + " " + validation_score,
+                source=source,
+                color=color,
+                alpha=0.7)
 
-            if 'voi_split' in run.validation_scores.get_score_names():
-
-                voi_sum = np.array(
-                    validation_averages['voi_split'] +
-                    validation_averages['voi_merge'])
-
-                x = run.validation_scores.iterations
-                source = bokeh.plotting.ColumnDataSource({
-                    'iteration': x,
-                    'voi_sum': voi_sum,
-                    'task': [run.task_config.id]*len(x),
-                    'model': [run.model_config.id]*len(x),
-                    'optimizer': [run.optimizer_config.id]*len(x),
-                    'voi_split': validation_averages['voi_split'],
-                    'voi_merge': validation_averages['voi_merge']
-                })
-                validation_figure.line(
-                    'iteration', 'voi_sum',
-                    legend_label=name + " VOI sum",
-                    source=source,
-                    color=color,
-                    alpha=0.7)
-
-                i = np.argmin(voi_sum)
-                source = bokeh.plotting.ColumnDataSource({
-                    'num_parameters': [run.model_config.num_parameters],
-                    'iteration': [x[i]],
-                    'voi_sum': [voi_sum[i]],
-                    'task': [run.task_config.id],
-                    'model': [run.model_config.id],
-                    'optimizer': [run.optimizer_config.id]
-                })
-                summary_figure.circle(
-                    'num_parameters', 'voi_sum',
-                    legend_label=name,
-                    source=source,
-                    color=color,
-                    alpha=0.7)
     bokeh.plotting.show(
         bokeh.layouts.column(
             loss_figure,
