3a2571fe585077d34aa194d7122d65cca7d8a134
Jan Funke <funkej@janelia.hhmi.org>
2020-03-16 13:35:06 -0400

    Add detection scores to label evaluation

--- again/evaluate/labels.py
+++ again/evaluate/labels.py
@@ -1,3 +1,4 @@
+import funlib.evaluate
 import numpy as np
 import zarr
 
@@ -6,6 +7,8 @@ def evaluate_labels(logits, gt_labels, store_results, background_label=None):
 
     pred_labels = np.argmax(logits.data, axis=0)
     gt_labels = gt_labels.data
+
+    # PIXEL-WISE SCORES
 
     sample_scores = {
         'ppv': 0,
@@ -50,15 +53,30 @@ def evaluate_labels(logits, gt_labels, store_results, background_label=None):
         tn = (gt_labels.size - num_relevant) - fp
 
         # precision, or positive predictive value
-        ppv = tp/num_selected  # = tp/(tp + fp)
+        if num_selected > 0:
+            ppv = tp/num_selected  # = tp/(tp + fp)
+        else:
+            ppv = np.nan
         # recall, or true positive rate
-        tpr = tp/num_relevant  # = tp/(tp + fn)
+        if num_relevant > 0:
+            tpr = tp/num_relevant  # = tp/(tp + fn)
+        else:
+            tpr = np.nan
         # specificity, or true negative rate
-        tnr = tn/(tn + fp)
+        if tn + fp > 0:
+            tnr = tn/(tn + fp)
+        else:
+            tnr = np.nan
         # fall-out, or false positive rate
-        fpr = fp/(tn + fp)
+        if tn + fp > 0:
+            fpr = fp/(tn + fp)
+        else:
+            fpr = np.nan
 
-        fscore = 2*(ppv*tpr)/(ppv + tpr)
+        if ppv + tpr > 0:
+            fscore = 2*(ppv*tpr)/(ppv + tpr)
+        else:
+            fscore = np.nan
         balanced_accuracy = (tpr + tnr)/2
 
         sample_scores[f'ppv_{label}'] = ppv
@@ -88,13 +106,67 @@ def evaluate_labels(logits, gt_labels, store_results, background_label=None):
     sample_scores[f'fpr'] /= num_classes
     sample_scores[f'fscore'] /= num_classes
     sample_scores[f'balanced_accuracy'] /= num_classes
-    if background_label is not None:
+    if background_label is not None and num_classes >= 2:
         sample_scores[f'ppv_fg'] /= num_classes - 1
         sample_scores[f'tpr_fg'] /= num_classes - 1
         sample_scores[f'tnr_fg'] /= num_classes - 1
         sample_scores[f'fpr_fg'] /= num_classes - 1
         sample_scores[f'fscore_fg'] /= num_classes - 1
         sample_scores[f'balanced_accuracy_fg'] /= num_classes - 1
+
+    # DETECTION SCORES (on foreground objects only)
+
+    # limit detection scores to foreground labels
+    if background_label is not None:
+        label_ids = label_ids[label_ids != background_label]
+    detection_scores = funlib.evaluate.detection_scores(
+        gt_labels,
+        pred_labels,
+        label_ids,
+        voxel_size=logits.spec.voxel_size)
+    for k, v in detection_scores.items():
+        sample_scores[f'detection_{k}'] = v
+
+    sample_scores['detection_ppv'] = 0.0
+    sample_scores['detection_tpr'] = 0.0
+    sample_scores['detection_fscore'] = 0.0
+    for label in label_ids:
+
+        tp = detection_scores[f'tp_{label}']
+        fp = detection_scores[f'fp_{label}']
+        fn = detection_scores[f'fn_{label}']
+        num_selected = tp + fp
+        num_relevant = tp + fn
+
+        # precision, or positive predictive value
+        if num_selected > 0:
+            ppv = tp/num_selected  # = tp/(tp + fp)
+        else:
+            ppv = np.nan
+        # recall, or true positive rate
+        if num_relevant > 0:
+            tpr = tp/num_relevant  # = tp/(tp + fn)
+        else:
+            tpr = np.nan
+
+        if num_relevant + num_selected > 0:
+            fscore = 2*(ppv*tpr)/(ppv + tpr)
+        else:
+            fscore = np.nan
+
+        sample_scores[f'detection_ppv_{label}'] = ppv
+        sample_scores[f'detection_tpr_{label}'] = tpr
+        sample_scores[f'detection_fscore_{label}'] = fscore
+
+        sample_scores['detection_ppv'] += ppv
+        sample_scores['detection_tpr'] += tpr
+        sample_scores['detection_fscore'] += fscore
+
+    num_classes = label_ids.size
+    if num_classes >= 1:
+        sample_scores['detection_ppv'] /= num_classes
+        sample_scores['detection_tpr'] /= num_classes
+        sample_scores['detection_fscore'] /= num_classes
 
     if store_results:
 
