e9b6f4d4070e833df31ab3112610d3c0a5fdf809
Jan Funke <jfunke@iri.upc.edu>
2020-04-26 17:55:02 -0400

    Add set_prediction to PostProcessor for potential speed-up

--- again/tasks/post_processors/post_processor.py
+++ again/tasks/post_processors/post_processor.py
@@ -6,12 +6,20 @@ class PostProcessor:
     def __init__(self, parameter_range):
         self.parameter_range = parameter_range
 
-    def process(self, prediction, parameters, store_results=None):
+    def set_prediction(self, prediction):
+        '''To be implemented in subclasses. This function will be called before
+        repeated calls to ``process`` and allows the post-processor to carry
+        out general post-processing that does not depend on parameters.'''
+        pass
+
+    def process(self, prediction, parameters):
         raise Exception("To be implemented in subclasses")
 
-    def enumerate(self, prediction, store_results=None):
+    def enumerate(self, prediction):
         '''Enumerate all parameter combinations and process the predictions.
         Yields tuples of ``(parameter, post_processed)``.'''
+
+        self.set_prediction(prediction)
 
         for parameters in self.parameter_range:
             if not self.reject_parameters(parameters):
@@ -19,8 +27,7 @@ class PostProcessor:
                 start = time.time()
                 post_processed = self.process(
                     prediction,
-                    parameters,
-                    store_results)
+                    parameters)
                 print(f"...done ({time.time() - start}s)")
                 yield parameters, post_processed
 
