388608353d7adc534bb09b35c970a3727a75ef09
pattonw <pattonw@hhmi.org>
2020-11-19 15:10:37 -0500

    ignore a label
    When using CrossEntropyLoss you may want to ignore a label.
    This can now be specified in the task config file under loss args.

--- dacapo/tasks/losses/cross_entropy_loss.py
+++ dacapo/tasks/losses/cross_entropy_loss.py
@@ -3,10 +3,9 @@ from .loss import Loss
 
 
 class CrossEntropyLoss(torch.nn.CrossEntropyLoss, Loss):
-
-    def __init__(self, weight=None):
+    def __init__(self, weight=None, ignore_label: int = -100):
 
         if weight is not None:
             weight = torch.tensor(weight)
 
-        super(CrossEntropyLoss, self).__init__(weight)
+        super(CrossEntropyLoss, self).__init__(weight, ignore_index=ignore_label)
