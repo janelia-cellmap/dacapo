005543c3e326183a42112f6bfc315000efec8e2f
pattonw <pattonw@hhmi.org>
2020-12-10 11:34:46 -0500

    formatting

--- dacapo/tasks/post_processors/post_processor.py
+++ dacapo/tasks/post_processors/post_processor.py
@@ -9,17 +9,17 @@ class PostProcessor:
         self.daisy_parameters = daisy_parameters if daisy_parameters is not None else {}
 
     def set_prediction(self, prediction):
-        '''To be implemented in subclasses. This function will be called before
+        """To be implemented in subclasses. This function will be called before
         repeated calls to ``process`` and allows the post-processor to carry
-        out general post-processing that does not depend on parameters.'''
+        out general post-processing that does not depend on parameters."""
         pass
 
     def process(self, prediction, parameters):
         raise Exception("To be implemented in subclasses")
 
     def enumerate(self, prediction):
-        '''Enumerate all parameter combinations and process the predictions.
-        Yields tuples of ``(parameter, post_processed)``.'''
+        """Enumerate all parameter combinations and process the predictions.
+        Yields tuples of ``(parameter, post_processed)``."""
 
         self.set_prediction(prediction)
 
@@ -27,13 +27,11 @@ class PostProcessor:
             if not self.reject_parameters(parameters):
                 print(f"Post-processing prediction with {parameters}...")
                 start = time.time()
-                post_processed = self.process(
-                    prediction,
-                    parameters)
+                post_processed = self.process(prediction, parameters)
                 print(f"...done ({time.time() - start}s)")
                 yield parameters, post_processed
 
     def reject_parameters(self, parameters):
-        '''To be implemented in subclasses to reject parameter configurations
-        that should be skipped.'''
+        """To be implemented in subclasses to reject parameter configurations
+        that should be skipped."""
         return False
