47af486005b525affddfc000afdfde715083619e
pattonw <pattonw@hhmi.org>
2021-01-12 09:23:42 -0500

    Convert evaluation inputs/outputs to daisy
    also reformat with black

--- dacapo/evaluate/labels.py
+++ dacapo/evaluate/labels.py
@@ -2,52 +2,56 @@ import funlib.evaluate
 import numpy as np
 
 import gunpowder as gp
+import daisy
 
 
 def evaluate_labels(
-        pred_labels,
-        gt_labels,
-        return_results=False,
-        background_label=None,
-        matching_score='overlap',
-        matching_threshold=1):
+    pred_labels,
+    gt_labels,
+    return_results=False,
+    background_label=None,
+    matching_score="overlap",
+    matching_threshold=1,
+):
 
-    voxel_size = gt_labels.spec.voxel_size
+    voxel_size = gt_labels.voxel_size
 
-    pred_labels_data = pred_labels.data
-    pred_labels_spec = pred_labels.spec.copy()
-    gt_labels_data = gt_labels.data
-    gt_labels_spec = gt_labels.spec.copy()
+    gt_labels_data = gt_labels.to_ndarray(roi=pred_labels.roi)
+    pred_labels_data = pred_labels.to_ndarray(roi=pred_labels.roi)
 
     # PIXEL-WISE SCORES
 
     sample_scores = {
-        'ppv': 0,
-        'tpr': 0,
-        'tnr': 0,
-        'fpr': 0,
-        'fscore': 0,
-        'balanced_accuracy': 0
+        "ppv": 0,
+        "tpr": 0,
+        "tnr": 0,
+        "fpr": 0,
+        "fscore": 0,
+        "balanced_accuracy": 0,
     }
 
     if background_label is not None:
-        sample_scores.update({
-            'ppv_fg': 0,
-            'tpr_fg': 0,
-            'tnr_fg': 0,
-            'fpr_fg': 0,
-            'fscore_fg': 0,
-            'balanced_accuracy_fg': 0
-        })
+        sample_scores.update(
+            {
+                "ppv_fg": 0,
+                "tpr_fg": 0,
+                "tnr_fg": 0,
+                "fpr_fg": 0,
+                "fscore_fg": 0,
+                "balanced_accuracy_fg": 0,
+            }
+        )
 
     # accuracy
 
-    sample_scores['accuracy'] = (pred_labels_data == gt_labels_data).sum()/gt_labels_data.size
+    sample_scores["accuracy"] = (
+        pred_labels_data == gt_labels_data
+    ).sum() / gt_labels_data.size
     if background_label is not None:
         fg_mask = gt_labels_data != background_label
-        sample_scores['fg_accuracy'] = (
-            pred_labels_data[fg_mask] ==
-            gt_labels_data[fg_mask]).sum()/fg_mask.sum()
+        sample_scores["fg_accuracy"] = (
+            pred_labels_data[fg_mask] == gt_labels_data[fg_mask]
+        ).sum() / fg_mask.sum()
 
     # precision, recall, fscore
 
@@ -65,65 +69,65 @@ def evaluate_labels(
 
         # precision, or positive predictive value
         if num_selected > 0:
-            ppv = tp/num_selected  # = tp/(tp + fp)
+            ppv = tp / num_selected  # = tp/(tp + fp)
         else:
             ppv = np.nan
         # recall, or true positive rate
         if num_relevant > 0:
-            tpr = tp/num_relevant  # = tp/(tp + fn)
+            tpr = tp / num_relevant  # = tp/(tp + fn)
         else:
             tpr = np.nan
         # specificity, or true negative rate
         if tn + fp > 0:
-            tnr = tn/(tn + fp)
+            tnr = tn / (tn + fp)
         else:
             tnr = np.nan
         # fall-out, or false positive rate
         if tn + fp > 0:
-            fpr = fp/(tn + fp)
+            fpr = fp / (tn + fp)
         else:
             fpr = np.nan
 
         if ppv + tpr > 0:
-            fscore = 2*(ppv*tpr)/(ppv + tpr)
+            fscore = 2 * (ppv * tpr) / (ppv + tpr)
         else:
             fscore = np.nan
-        balanced_accuracy = (tpr + tnr)/2
+        balanced_accuracy = (tpr + tnr) / 2
 
-        sample_scores[f'ppv_{label}'] = ppv
-        sample_scores[f'tpr_{label}'] = tpr
-        sample_scores[f'tnr_{label}'] = tnr
-        sample_scores[f'fpr_{label}'] = fpr
-        sample_scores[f'fscore_{label}'] = fscore
-        sample_scores[f'balanced_accuracy_{label}'] = balanced_accuracy
-        sample_scores[f'ppv'] += ppv
-        sample_scores[f'tpr'] += tpr
-        sample_scores[f'tnr'] += tnr
-        sample_scores[f'fpr'] += fpr
-        sample_scores[f'fscore'] += fscore
-        sample_scores[f'balanced_accuracy'] += balanced_accuracy
+        sample_scores[f"ppv_{label}"] = ppv
+        sample_scores[f"tpr_{label}"] = tpr
+        sample_scores[f"tnr_{label}"] = tnr
+        sample_scores[f"fpr_{label}"] = fpr
+        sample_scores[f"fscore_{label}"] = fscore
+        sample_scores[f"balanced_accuracy_{label}"] = balanced_accuracy
+        sample_scores["ppv"] += ppv
+        sample_scores["tpr"] += tpr
+        sample_scores["tnr"] += tnr
+        sample_scores["fpr"] += fpr
+        sample_scores["fscore"] += fscore
+        sample_scores["balanced_accuracy"] += balanced_accuracy
         if background_label is not None and label != background_label:
-            sample_scores[f'ppv_fg'] += ppv
-            sample_scores[f'tpr_fg'] += tpr
-            sample_scores[f'tnr_fg'] += tnr
-            sample_scores[f'fpr_fg'] += fpr
-            sample_scores[f'fscore_fg'] += fscore
-            sample_scores[f'balanced_accuracy_fg'] += balanced_accuracy
+            sample_scores["ppv_fg"] += ppv
+            sample_scores["tpr_fg"] += tpr
+            sample_scores["tnr_fg"] += tnr
+            sample_scores["fpr_fg"] += fpr
+            sample_scores["fscore_fg"] += fscore
+            sample_scores["balanced_accuracy_fg"] += balanced_accuracy
 
     num_classes = label_ids.size
-    sample_scores[f'ppv'] /= num_classes
-    sample_scores[f'tpr'] /= num_classes
-    sample_scores[f'tnr'] /= num_classes
-    sample_scores[f'fpr'] /= num_classes
-    sample_scores[f'fscore'] /= num_classes
-    sample_scores[f'balanced_accuracy'] /= num_classes
+    sample_scores["ppv"] /= num_classes
+    sample_scores["tpr"] /= num_classes
+    sample_scores["tnr"] /= num_classes
+    sample_scores["fpr"] /= num_classes
+    sample_scores["fscore"] /= num_classes
+    sample_scores["balanced_accuracy"] /= num_classes
     if background_label is not None and num_classes >= 2:
-        sample_scores[f'ppv_fg'] /= num_classes - 1
-        sample_scores[f'tpr_fg'] /= num_classes - 1
-        sample_scores[f'tnr_fg'] /= num_classes - 1
-        sample_scores[f'fpr_fg'] /= num_classes - 1
-        sample_scores[f'fscore_fg'] /= num_classes - 1
-        sample_scores[f'balanced_accuracy_fg'] /= num_classes - 1
+        sample_scores["ppv_fg"] /= num_classes - 1
+        sample_scores["tpr_fg"] /= num_classes - 1
+        sample_scores["tnr_fg"] /= num_classes - 1
+        sample_scores["fpr_fg"] /= num_classes - 1
+        sample_scores["fscore_fg"] /= num_classes - 1
+        sample_scores["balanced_accuracy_fg"] /= num_classes - 1
 
     # DETECTION SCORES (on foreground objects only)
 
@@ -137,59 +141,60 @@ def evaluate_labels(
         matching_score,
         matching_threshold,
         voxel_size=voxel_size,
-        return_matches=return_results)
+        return_matches=return_results,
+    )
     for k, v in detection_scores.items():
-        sample_scores[f'detection_{k}'] = v
+        sample_scores[f"detection_{k}"] = v
     non_score_keys = []
     for k in sample_scores.keys():
-        if k.startswith('detection_components_'):
+        if k.startswith("detection_components_"):
             non_score_keys.append(k)
-        elif k.startswith('detection_matches_'):
+        elif k.startswith("detection_matches_"):
             non_score_keys.append(k)
     for k in non_score_keys:
         del sample_scores[k]
 
-    sample_scores['detection_ppv'] = 0.0
-    sample_scores['detection_tpr'] = 0.0
-    sample_scores['detection_fscore'] = 0.0
+    sample_scores["detection_ppv"] = 0.0
+    sample_scores["detection_tpr"] = 0.0
+    sample_scores["detection_fscore"] = 0.0
     components = {}
     for label in label_ids:
 
-        tp = detection_scores[f'tp_{label}']
-        fp = detection_scores[f'fp_{label}']
-        fn = detection_scores[f'fn_{label}']
+        tp = detection_scores[f"tp_{label}"]
+        fp = detection_scores[f"fp_{label}"]
+        fn = detection_scores[f"fn_{label}"]
         num_selected = tp + fp
         num_relevant = tp + fn
 
         # precision, or positive predictive value
         if num_selected > 0:
-            ppv = tp/num_selected  # = tp/(tp + fp)
+            ppv = tp / num_selected  # = tp/(tp + fp)
         else:
             ppv = np.nan
         # recall, or true positive rate
         if num_relevant > 0:
-            tpr = tp/num_relevant  # = tp/(tp + fn)
+            tpr = tp / num_relevant  # = tp/(tp + fn)
         else:
             tpr = np.nan
 
         if ppv + tpr > 0:
-            fscore = 2*(ppv*tpr)/(ppv + tpr)
+            fscore = 2 * (ppv * tpr) / (ppv + tpr)
         else:
             fscore = np.nan
 
-        sample_scores[f'detection_ppv_{label}'] = ppv
-        sample_scores[f'detection_tpr_{label}'] = tpr
-        sample_scores[f'detection_fscore_{label}'] = fscore
+        sample_scores[f"detection_ppv_{label}"] = ppv
+        sample_scores[f"detection_tpr_{label}"] = tpr
+        sample_scores[f"detection_fscore_{label}"] = fscore
 
-        sample_scores['detection_ppv'] += ppv
-        sample_scores['detection_tpr'] += tpr
-        sample_scores['detection_fscore'] += fscore
+        sample_scores["detection_ppv"] += ppv
+        sample_scores["detection_tpr"] += tpr
+        sample_scores["detection_fscore"] += fscore
 
         if return_results:
 
-            components_gt = detection_scores[f'components_truth_{label}']
-            components_pred = detection_scores[f'components_test_{label}']
-            matches = detection_scores[f'matches_{label}']
+            components_gt = detection_scores[f"components_truth_{label}"]
+            components_pred = detection_scores[f"components_test_{label}"]
+            matches = detection_scores[f"matches_{label}"]
             matches_gt = np.array([m[1] for m in matches])
             matches_pred = np.array([m[0] for m in matches])
             components_tp_gt = np.copy(components_gt)
@@ -203,43 +208,38 @@ def evaluate_labels(
             components_fn_gt[tp_gt_mask] = 0
             components_fp_pred[tp_pred_mask] = 0
 
-            components_spec = gt_labels_spec.copy()
-            components_spec.dtype = components_tp_gt.dtype
+            components[f"volumes/components_tp_gt_{label}"] = daisy.Array(
+                components_tp_gt, pred_labels.roi, voxel_size
+            )
+            components[f"volumes/components_fn_gt_{label}"] = daisy.Array(
+                components_fn_gt, pred_labels.roi, voxel_size
+            )
+            components[f"volumes/components_tp_pred_{label}"] = daisy.Array(
+                components_tp_pred, pred_labels.roi, voxel_size
+            )
+            components[f"volumes/components_fp_pred_{label}"] = daisy.Array(
+                components_fp_pred, pred_labels.roi, voxel_size
+            )
 
-            components[f"components_tp_gt_{label}"] = gp.Array(
-                components_tp_gt, components_spec.copy()
-            )
-            components[f"components_fn_gt_{label}"] = gp.Array(
-                components_fn_gt, components_spec.copy()
-            )
-            components[f"components_tp_pred_{label}"] = gp.Array(
-                components_tp_pred, components_spec.copy()
-            )
-            components[f"components_fp_pred_{label}"] = gp.Array(
-                components_fp_pred, components_spec.copy()
-            )
-            
     num_classes = label_ids.size
     if num_classes >= 1:
-        sample_scores['detection_ppv'] /= num_classes
-        sample_scores['detection_tpr'] /= num_classes
-        sample_scores['detection_fscore'] /= num_classes
+        sample_scores["detection_ppv"] /= num_classes
+        sample_scores["detection_tpr"] /= num_classes
+        sample_scores["detection_fscore"] /= num_classes
 
-    scores = {'sample': sample_scores, 'average': sample_scores}
+    scores = {"sample": sample_scores, "average": sample_scores}
 
     if return_results:
-        pred_labels_spec.dtype = np.uint64
-        gt_labels_spec.dtype = np.uint64
         results = {
-            "pred_labels": gp.Array(
-                pred_labels_data.astype(np.uint64), pred_labels_spec
+            "volumes/pred_labels": daisy.Array(
+                pred_labels_data.astype(np.uint64), pred_labels.roi, voxel_size
             ),
-            "gt_labels": gp.Array(gt_labels_data.astype(np.uint64), gt_labels_spec),
+            "volumes/gt_labels": daisy.Array(
+                gt_labels_data.astype(np.uint64), pred_labels.roi, voxel_size
+            ),
         }
         for k, v in components.items():
-            spec = v.spec
-            spec.dtype = np.uint64
-            v_uint64 = gp.Array(v.data.astype(np.uint64), spec)
+            v_uint64 = daisy.Array(v.data.astype(np.uint64), v.roi, voxel_size)
             results[k] = v_uint64
 
         return scores, results
