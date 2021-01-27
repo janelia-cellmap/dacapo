cac88c366c74ea539a7c7d796ce799e99f3a84d2
pattonw <pattonw@hhmi.org>
2021-01-27 13:08:45 -0500

    Add a distance weighted MSE loss

--- dacapo/tasks/losses/weighted_mse_loss.py
+++ dacapo/tasks/losses/weighted_mse_loss.py
@@ -2,20 +2,34 @@ from .loss import Loss
 import gunpowder as gp
 import torch
 
+from .gp import AddDistance
+
 
 class WeightedMSELoss(torch.nn.MSELoss, Loss):
-
     def __init__(self):
         super(WeightedMSELoss, self).__init__()
 
     def add_weights(self, target, weights):
 
-        return gp.BalanceLabels(
-            labels=target,
-            scales=weights)
+        return gp.BalanceLabels(labels=target, scales=weights)
 
     def forward(self, prediction, target, weights):
 
         return super(WeightedMSELoss, self).forward(
-            prediction*weights,
-            target*weights)
+            prediction * weights, target * weights
+        )
+
+
+class DistanceWeightedMSELoss(torch.nn.MSELoss, Loss):
+    def __init__(self):
+        super(DistanceWeightedMSELoss, self).__init__()
+
+    def add_weights(self, target, weights):
+
+        return AddDistance(labels=target, scales=weights)
+
+    def forward(self, prediction, target, weights):
+
+        return super(DistanceWeightedMSELoss, self).forward(
+            prediction * weights, target * weights
+        )
