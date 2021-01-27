f20ded75724dcadd40f9803f9d1cff2cca4a6d3c
pattonw <pattonw@hhmi.org>
2021-01-27 13:06:05 -0500

    add AddDistance node to __init__.py

--- dacapo/gp/__init__.py
+++ dacapo/gp/__init__.py
@@ -2,5 +2,6 @@ from .add_channel_dim import AddChannelDim  # noqa
 from .remove_channel_dim import RemoveChannelDim  # noqa
 from .squash import Squash  # noqa
 from .transpose_dims import TransposeDims  # noqa
-from .train_multi_head import Train # noqa
-from .binarize import BinarizeNot # noqa
+from .train_multi_head import Train  # noqa
+from .binarize import BinarizeNot  # noqa
+from .balance_labels import AddDistance  # noqa
