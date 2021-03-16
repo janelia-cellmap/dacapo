7a47e18d7992979f60b27b4430769e504239d819
pattonw <pattonw@hhmi.org>
2021-03-16 11:18:09 -0400

    update __init__.py

--- dacapo/__init__.py
+++ dacapo/__init__.py
@@ -1,4 +1,7 @@
 from .hash import human_readable_hash  # noqa
-from .run import enumerate_runs, run_local, run_all, Run  # noqa
+from .run import enumerate_runs, run_local, run_all  # noqa
+from .validate import validate_local  # noqa
 
 from . import analyze  # noqa
+from . import configurables  # noqa
+from . import config_fields  # noqa
