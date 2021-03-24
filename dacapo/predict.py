686c931d069582c65ab62ab1c423cab617ef5b15
pattonw <pattonw@hhmi.org>
2021-03-24 08:45:06 -0400

    overhaul predict api

--- dacapo/predict.py
+++ dacapo/predict.py
@@ -1,256 +1,281 @@
 from dacapo.tasks import Task
-from dacapo.data import PredictData, Data
-from dacapo.predict_pipeline import predict, predict_pipeline
+from dacapo.data import DataSource
 from dacapo.store import MongoDbStore
+from .daisy import predict
 
 import daisy
-import zarr
-import numpy as np
-
+from funlib.geometry import Coordinate, Roi
 import funlib.run
-import torch
+import attr
+
+from dacapo.data import Dataset, ArrayDataSource
+from dacapo.models import Model
 
 from pathlib import Path
-import time
+from typing import List, Optional
 import logging
+import subprocess
+import os
 
 
 logger = logging.getLogger(__name__)
 
 
-class PredictRun:
-    def __init__(self, run, predict_data, daisy_worker=False, model_padding=None):
+@attr.s
+class PredictConfig:
+    run_id: str = attr.ib()
+    pred_id: str = attr.ib()
+    dataset_id: str = attr.ib()
+    data_source: str = attr.ib()
+    out_container: Path = attr.ib()
+    backbone_checkpoint: Path = attr.ib()
+    head_checkpoints: List[Path] = attr.ib()
+    gt: Optional[ArrayDataSource] = attr.ib()
+    raw: ArrayDataSource = attr.ib()
+    input_shape: Coordinate = attr.ib()
+    output_shape: Coordinate = attr.ib()
+    num_workers: int = attr.ib(default=1)
 
-        # configs
-        self.run = run
-        self.predict_data = predict_data
-        self.daisy_worker = daisy_worker
-        self.model_padding = model_padding
+    @property
+    def output_dir(self):
+        return Path(self.out_container).parent
 
-        self.steps = [0]
+    @property
+    def output_filename(self):
+        return Path(self.out_container).name
 
-    def start(self):
+    @property
+    def voxel_size(self):
+        assert self.gt.voxel_size == self.raw.voxel_size
+        return self.raw.voxel_size
 
-        # set torch flags:
-        # TODO: make these configurable?
-        torch.backends.cudnn.enabled = True
-        torch.backends.cudnn.benchmark = True
+    @property
+    def input_size(self):
+        return self.input_shape * self.voxel_size
 
-        self.started = time.time()
+    @property
+    def output_size(self):
+        return self.output_shape * self.voxel_size
 
-        data = PredictData(self.predict_data)
-        run_data = Data(self.run.data_config)
-        model = self.run.model_config.type(data, self.run.model_config)
-        task = Task(run_data, model, self.run.task_config)
+    @property
+    def context(self):
+        return (self.input_size - self.output_size) / 2
 
-        self.__load_best_state(self.run, model, task)
+    @property
+    def input_roi(self):
+        if self.gt is None:
+            input_roi = self.raw.roi
+        else:
+            output_roi = self.gt.roi
+            input_roi = output_roi.grow(self.context, self.context)
+        return input_roi
 
-        predict(
-            data.raw.test,
-            model,
-            task.predictor,
-            run_hash=self.run.hash,
-            output_dir=f"predictions/{self.run.hash}",
-            output_filename="data.zarr",
-            gt=None,
-            aux_tasks=[],
-            total_roi=data.total_roi,
-            daisy_worker=self.daisy_worker,
-            model_padding=self.model_padding,
-            checkpoint=f"{self.run.best_checkpoint}",
-            padding_mode=data.prediction_padding,
+    @property
+    def output_roi(self):
+        if self.gt is None:
+            input_roi = self.raw.roi
+            output_roi = input_roi.grow(-self.context, -self.context)
+        else:
+            output_roi = self.gt.roi
+        return output_roi
+
+    @property
+    def task_id(self):
+        return f"{self.run_id}_{self.pred_id}"
+
+    def task(self):
+        store = MongoDbStore()
+
+        print(
+            {
+                "total_roi": self.input_roi,
+                "read_roi": Roi((0,) * self.input_size.dims, self.input_size),
+                "write_roi": Roi(self.context, self.output_size),
+            }
+        )
+        return daisy.Task(
+            task_id=self.task_id,
+            total_roi=self.input_roi,
+            read_roi=Roi((0,) * self.input_size.dims, self.input_size),
+            write_roi=Roi(self.context, self.output_size),
+            process_function=remote_prediction_worker(
+                self.run_id,
+                self.pred_id,
+                self.dataset_id,
+                self.data_source,
+                self.out_container,
+                self.backbone_checkpoint,
+                self.head_checkpoints,
+            ),
+            check_function=lambda b: store.check_block(
+                self.task_id, "prediction", b.block_id
+            ),
+            num_workers=self.num_workers,
+            fit="overhang",
         )
 
-        self.stopped = time.time()
 
-        logger.info(f"prediction took {self.started - self.stopped} seconds!")
-
-    def __load_best_state(self, run, model, task):
-        filename = Path(
-            run.outdir,
-            f"validation_best_{run.best_score_name}.checkpoint",
-        )
-        run._load_parameters(filename, model, task.heads, optimizer=None)
-
-
-def run_local(run, data, daisy_config):
-    predict = PredictRun(
-        run, data, daisy_config.worker, getattr(daisy_config, "model_padding", None)
-    )
-
-    print(f"Running run {predict.run.hash} with data {predict.predict_data}")
-
-    predict.start()
-
-
-def process_block(outdir, block, fail=None):
-
-    logger.debug("Processing block %s", block)
-
-    if block.block_id == fail:
-        raise RuntimeError("intended failure")
-
-    path = Path(outdir, "%d.block" % block.block_id)
-    with open(path, "w") as f:
-        f.write(str(block.block_id))
-
-    return 0
-
-
-def predict_worker(dacapo_flags, bsub_flags, outdir):
-
-    worker_id = daisy.Context.from_env().worker_id
-
-    log_out = f"{outdir}/out_{worker_id}.log"
-    log_err = f"{outdir}/out_{worker_id}.err"
-    command = [f"dacapo predict {dacapo_flags}"]
-
-    daisy.call(command, log_out=log_out, log_err=log_err)
-
-    logging.warning("Predict worker finished")
-
-
-def run_remote(run, data, daisy_config, dacapo_flags, bsub_flags):
-    training_data = Data(run.data_config)
-    predict_data = PredictData(data)
-    model = run.model_config.type(training_data, run.model_config)
-    task = Task(training_data, model, run.task_config)
-    post_processor = task.predictor.post_processor
-
-    # build pipeline to prepare datasets
-    compute_pipeline, sources, total_request = predict_pipeline(
-        predict_data.raw,
-        model,
-        task.predictor,
-        run_hash=run.hash,
-        output_dir=f"predictions/{run.hash}",
-        output_filename="data.zarr",
-        gt=None,
-        aux_tasks=task.aux_tasks,
-        total_roi=predict_data.total_roi,
-        model_padding=getattr(daisy_config, "model_padding", None),
-        daisy_worker=False,
-    )
-
-    outdir = f"predictions/{run.hash}"
-    if not Path(outdir).exists():
-        Path(outdir).mkdir()
-
-    voxel_size = np.array(predict_data.raw.test.voxel_size, dtype=int)
-    input_size = np.array(model.input_shape, dtype=int) * voxel_size
-    output_size = np.array(model.output_shape, dtype=int) * voxel_size
-    context = (input_size - output_size) // 2
-    offset = input_size * 0
-    if hasattr(daisy_config, "model_padding"):
-        model_padding = daisy_config.model_padding * voxel_size
-
-        input_block_roi = daisy.Roi(tuple(offset), tuple(input_size + model_padding))
-        output_block_roi = daisy.Roi(tuple(context), tuple(output_size + model_padding))
-    else:
-        input_block_roi = daisy.Roi(tuple(offset), tuple(input_size))
-        output_block_roi = daisy.Roi(tuple(context), tuple(output_size))
-
-    logger.warning("Starting blockwise prediction")
-
+def load_prediction_config(
+    run_id,
+    prediction_id,
+    dataset_id,
+    data_source,
+    out_container,
+    backbone_checkpoint,
+    head_checkpoints,
+):
+    # Load Predict Config from cli arguments
     store = MongoDbStore()
-    step_id = "prediction"
-    prediction_id = f"{run.hash}_predict"
+    # get necessary configs from job
+    run = store.get_run(run_id)
+    dataset = store.get_dataset(run.dataset)
+    model = store.get_model(run.model)
 
-    total_roi = daisy.Roi(
-        predict_data.raw.roi.get_offset(), predict_data.raw.roi.get_shape()
+    input_shape = Coordinate(model.input_shape)
+    output_shape = (
+        Coordinate(model.output_shape) if model.output_shape is not None else None
     )
 
-    daisy.run_blockwise(
-        total_roi,
-        input_block_roi,
-        output_block_roi,
-        process_function=lambda: predict_worker(dacapo_flags, bsub_flags, outdir),
-        check_function=lambda b: store.check_block(prediction_id, step_id, b.block_id),
-        num_workers=daisy_config.num_workers,
-        read_write_conflict=False,
-        fit="overhang",
+    backbone = model.instantiate(dataset)
+    if output_shape is None:
+        output_shape = Coordinate(backbone.output_shape(input_shape))
+
+    # load in data to run on
+    dataset = store.get_dataset(dataset_id)
+    assert hasattr(dataset, "raw") and hasattr(
+        dataset.raw, data_source
+    ), f"{dataset} has no raw.{data_source}"
+    raw = getattr(dataset.raw, data_source)
+    try:
+        gt = getattr(dataset.gt, data_source)
+    except AttributeError:
+        gt = None
+
+    # switch to world units
+    voxel_size = raw.voxel_size
+    input_size = voxel_size * input_shape
+    output_size = voxel_size * output_shape
+
+    # calculate input and output rois
+    context = (input_size - output_size) / 2
+    logger.warning(f"context: {context}")
+
+    if gt is None:
+        input_roi = raw.roi
+        output_roi = input_roi.grow(-context, -context)
+    else:
+        output_roi = gt.roi
+        input_roi = output_roi.grow(context, context)
+
+    return PredictConfig(
+        run_id,
+        prediction_id,
+        dataset_id,
+        data_source,
+        out_container,
+        backbone_checkpoint,
+        head_checkpoints,
+        gt,
+        raw,
+        input_shape,
+        output_shape,
     )
-    logger.warning("Finished blockwise prediction")
 
-    for name, fit, step, datasets in post_processor.daisy_steps():
-        predictions = daisy.open_ds(
-            f"predictions/{run.hash}/data.zarr",
-            "volumes/prediction",
-        )
-        post_processing_parameters = post_processor.daisy_parameters[name]
 
-        if "total_roi" in post_processing_parameters:
-            step_total_roi = daisy.Roi(
-                *post_processing_parameters.pop("total_roi")
-            )
-            step_total_roi = total_roi.intersect(step_total_roi)
-        elif "total_roi_context" in post_processing_parameters:
-            total_roi_context = daisy.Coordinate(
-                post_processing_parameters.pop("total_roi_context")
-            )
-            step_total_roi = total_roi.grow(-total_roi_context, -total_roi_context)
-        else:
-            step_total_roi = total_roi
+def predict_one(
+    run_id,
+    prediction_id,
+    dataset_id,
+    data_source,
+    out_container,
+    backbone_checkpoint,
+    head_checkpoints,
+):
+    # Command line entrypoint to generating a prediction.
+    # This is a blocking call, will only return when prediction
+    # is completed.
+    # This call creates a daisy task and spins up a set of predict workers
 
-        if fit != "global":
-            logger.warning(f"Starting blockwise {name}")
-            if "write_shape" in post_processing_parameters:
-                write_shape = post_processing_parameters.pop("write_shape")
-                context = post_processing_parameters.pop("context")
-                step_output_block_roi = daisy.Roi(context, write_shape)
-                step_input_block_roi = step_output_block_roi.grow(context, context)
-            else:
-                step_output_block_roi = output_block_roi
-                step_input_block_roi = input_block_roi
+    # There is no support for predicting on a sub_roi of a volumes yet.
+    # If you need this, you should copy the data you want to predict into a
+    # seperate zarr dataset and then predict on it.
+    logger.warning("Predicting on validation data...")
 
-            for dataset in datasets:
-                output_roi = step_total_roi.grow(
-                    -daisy.Coordinate(context), -daisy.Coordinate(context)
-                )
-                daisy.prepare_ds(
-                    f"predictions/{run.hash}/data.zarr",
-                    dataset,
-                    output_roi,
-                    predictions.voxel_size,
-                    dtype=np.uint64,
-                    write_size=step_output_block_roi.get_shape(),
-                    compressor=zarr.storage.default_compressor.get_config(),
-                )
+    Path(out_container).mkdir(parents=True, exist_ok=True)
 
-            if "num_workers" in post_processing_parameters:
-                num_workers = post_processing_parameters.pop("num_workers")
-            else:
-                num_workers = daisy_config.num_workers
+    predict_config = load_prediction_config(
+        run_id,
+        prediction_id,
+        dataset_id,
+        data_source,
+        out_container,
+        backbone_checkpoint,
+        head_checkpoints,
+    )
 
-            success = daisy.run_blockwise(
-                step_total_roi,
-                step_input_block_roi,
-                step_output_block_roi,
-                process_function=lambda: step(
-                    run_hash=run.hash,
-                    output_dir=f"predictions/{run.hash}",
-                    output_filename="data.zarr",
-                    **post_processing_parameters,
-                ),
-                check_function=lambda b: store.check_block(
-                    f"{run.hash}_{name}", step_id, b.block_id
-                ),
-                num_workers=num_workers,
-                read_write_conflict=False,
-                fit=fit,
-            )
-            if not success:
-                logger.warning(f"Failed blockwsie {name}")
-                return
-            logger.warning(f"Finished blockwise {name}")
-        else:
-            logger.warning(f"Starting global {name}")
-            success = step(
-                run_hash=run.hash,
-                roi=step_total_roi,
-                **post_processing_parameters,
-            )
-            if not success:
-                logger.warning(f"Failed global {name}")
-                return
-            logger.warning(f"Finished global {name}")
+    predict_task = predict_config.task()
+    return daisy.run_blockwise([predict_task])
+
+
+def predict_worker(
+    run_id,
+    prediction_id,
+    dataset_id,
+    data_source,
+    out_container,
+    backbone_checkpoint,
+    head_checkpoints,
+):
+    logger.warning("Predicting in worker... ")
+
+    Path(out_container).mkdir(parents=True, exist_ok=True)
+
+    logger.warning("Loading config... ")
+
+    predict_config = load_prediction_config(
+        run_id,
+        prediction_id,
+        dataset_id,
+        data_source,
+        out_container,
+        backbone_checkpoint,
+        head_checkpoints,
+    )
+    logger.warning("Config loaded... ")
+    predict(predict_config)
+
+
+def remote_prediction_worker(
+    run_id,
+    prediction_id,
+    dataset_id,
+    data_source,
+    out_container,
+    backbone_checkpoint,
+    head_checkpoints,
+):
+    store = MongoDbStore()
+    run = store.get_run(run_id)
+    out_dir = Path(out_container).parent
+
+    command_args = funlib.run.run(
+        command=f"dacapo predict-worker -r {run_id} -p {prediction_id} "
+        f"-d {dataset_id} -ds {data_source} -oc {out_container} "
+        f"-bb {backbone_checkpoint} "
+        + " ".join(f"-hs {head_checkpoint}" for head_checkpoint in head_checkpoints),
+        num_cpus=5,
+        num_gpus=1,
+        queue="gpu_rtx",
+        execute=False,
+        expand=False,
+        flags=run.execution_details.bsub_flags.split(" "),
+        batch=run.execution_details.batch,
+        log_file=f"{out_dir}/log.out",
+        error_file=f"{out_dir}/log.err",
+    )
+
+    def process_function():
+        print("starting worker:")
+        subprocess.run(" ".join(command_args), shell=True, encoding="UTF-8")
+
+    return process_function
