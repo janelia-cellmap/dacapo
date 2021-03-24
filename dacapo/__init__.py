05dd6306103a7ec1aa0628abc5da49374923499d
pattonw <pattonw@hhmi.org>
2021-03-24 08:54:38 -0400

    add prediction commands to cli

--- dacapo/__init__.py
+++ dacapo/__init__.py
@@ -1,6 +1,6 @@
 from .hash import human_readable_hash  # noqa
 from .run import enumerate_runs, run_local, run_all  # noqa
-from .validate import validate_local  # noqa
+from .predict import predict_one, predict_worker  # noqa
 
 from . import analyze  # noqa
 from . import configurables  # noqa
