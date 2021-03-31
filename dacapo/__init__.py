0826edffa692e1a3df0e026203864d5959e2ae05
pattonw <pattonw@hhmi.org>
2021-03-31 15:34:31 -0400

    add post_process_functions

--- dacapo/__init__.py
+++ dacapo/__init__.py
@@ -1,6 +1,11 @@
 from .run import enumerate_runs, run_local, run_all  # noqa
 from .predict import predict_one, predict_worker  # noqa
 from .validate import validate_one  # noqa
+from .post_process import (
+    post_process_one,
+    post_process_local,
+    post_process_remote,
+)  # noqa
 
 from . import analyze  # noqa
 from . import configurables  # noqa
