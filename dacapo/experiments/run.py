e99015c8ca665e8b412003583846531ce6806672
Jan Funke <funkej@janelia.hhmi.org>
2021-06-17 16:39:53 -0400

    Create model and optimizer in Run constructor

--- dacapo/experiments/run.py
+++ dacapo/experiments/run.py
@@ -7,16 +7,21 @@ class Run:
 
     def __init__(self, run_config):
 
+        self.name = run_config.name
+
         task_type = run_config.task_config.task_type
         architecture_type = run_config.architecture_config.architecture_type
         trainer_type = run_config.trainer_config.trainer_type
-
-        self.name = run_config.name
 
         self.task = task_type(run_config.task_config)
         self.architecture = architecture_type(run_config.architecture_config)
         self.trainer = trainer_type(run_config.trainer_config)
         self.dataset = Dataset(run_config.dataset_config)
+
+        self.model = self.task.predictor.create_model(
+            self.architecture,
+            self.dataset)
+        self.optimizer = self.trainer.create_optimizer(self.model)
 
         self.training_stats = TrainingStats()
         self.validation_scores = ValidationScores()
