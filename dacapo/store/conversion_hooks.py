ef302a3ca500ca87b21e9745733aaa5fa38e6346
Jan Funke <funkej@janelia.hhmi.org>
2021-06-17 09:05:05 -0400

    Add conversion hooks for EvaluationScores, PostProcessorParameters

--- dacapo/store/conversion_hooks.py
+++ dacapo/store/conversion_hooks.py
@@ -1,11 +1,13 @@
 # star imports ensure visibility of concrete classes, so here they are accepted
 # flake8: noqa: F405
-from dacapo.experiments.tasks import *
 from dacapo.experiments.architectures import *
-from dacapo.experiments.trainers import *
 from dacapo.experiments.datasets import *
 from dacapo.experiments.datasets.dataset_config import DataSourceConfigs
 from dacapo.experiments.datasets.keys import *
+from dacapo.experiments.tasks import *
+from dacapo.experiments.tasks.evaluators import *
+from dacapo.experiments.tasks.post_processors import *
+from dacapo.experiments.trainers import *
 from pathlib import Path
 
 def register_hierarchy_hooks(converter):
@@ -16,7 +18,8 @@ def register_hierarchy_hooks(converter):
     converter.register_hierarchy(TrainerConfig, cls_fun)
     converter.register_hierarchy(ArraySourceConfig, cls_fun)
     converter.register_hierarchy(GraphSourceConfig, cls_fun)
-    # converter.register_hierarchy(DataKey, cls_fun)
+    converter.register_hierarchy(EvaluationScores, cls_fun)
+    converter.register_hierarchy(PostProcessorParameters, cls_fun)
 
 def register_hooks(converter):
     """Central place to register all conversion hooks with the given
