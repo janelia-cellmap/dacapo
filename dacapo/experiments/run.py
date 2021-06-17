4c6748362c384d83ac192ad8eadc13ca891dd5c9
Jan Funke <funkej@janelia.hhmi.org>
2021-06-17 09:07:01 -0400

    Add training/validation stats to Run

--- dacapo/experiments/run.py
+++ dacapo/experiments/run.py
@@ -1,4 +1,6 @@
 from .datasets import Dataset
+from .training_stats import TrainingStats
+from .validation_scores import ValidationScores
 
 
 class Run:
@@ -9,7 +11,12 @@ class Run:
         architecture_type = run_config.architecture_config.architecture_type
         trainer_type = run_config.trainer_config.trainer_type
 
+        self.name = run_config.name
+
         self.task = task_type(run_config.task_config)
         self.architecture = architecture_type(run_config.architecture_config)
         self.trainer = trainer_type(run_config.trainer_config)
         self.dataset = Dataset(run_config.dataset_config)
+
+        self.training_stats = TrainingStats()
+        self.validation_scores = ValidationScores()
