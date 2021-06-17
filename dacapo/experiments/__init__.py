d77500091cf6274d96793425730079b0deff5482
Jan Funke <funkej@janelia.hhmi.org>
2021-06-17 11:46:21 -0400

    Add Model class

--- dacapo/experiments/__init__.py
+++ dacapo/experiments/__init__.py
@@ -1,6 +1,7 @@
+from .model import Model  # noqa
 from .run import Run  # noqa
 from .run_config import RunConfig  # noqa
-from .training_stats import TrainingStats  # noqa
 from .training_iteration_stats import TrainingIterationStats  # noqa
-from .validation_scores import ValidationScores  # noqa
+from .training_stats import TrainingStats  # noqa
 from .validation_iteration_scores import ValidationIterationScores  # noqa
+from .validation_scores import ValidationScores  # noqa
