73d027ac8350ce5c7d03821afec1af2760afb64d
pattonw <pattonw@hhmi.org>
2021-10-14 23:31:29 -0400

    add file based config_store and stats_store

---
+++ dacapo/store/file_config_store.py
@@ -0,0 +1,163 @@
+from .config_store import ConfigStore, DuplicateNameError
+from .converter import converter
+from dacapo.experiments import RunConfig
+from dacapo.experiments.architectures import ArchitectureConfig
+from dacapo.experiments.datasplits import DataSplitConfig
+from dacapo.experiments.tasks import TaskConfig
+from dacapo.experiments.trainers import TrainerConfig
+from pymongo import MongoClient, ASCENDING
+from pymongo.errors import DuplicateKeyError
+
+import logging
+import pickle
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+class FileConfigStore(ConfigStore):
+    """A Local File based store for configurations. Used to store and retrieve
+    configurations for runs, tasks, architectures, trainers, and datasplits.
+    """
+
+    def __init__(self, path):
+
+        logger.info("Creating FileConfigStore:\n\tpath    : %s", path)
+
+        self.path = Path(path)
+
+        self.__open_collections()
+        self.__init_db()
+
+    def store_run_config(self, run_config):
+
+        run_doc = converter.unstructure(run_config)
+        self.__save_insert(self.runs, run_doc)
+
+    def retrieve_run_config(self, run_name):
+
+        run_doc = self.__load(self.runs, run_name)
+        return converter.structure(run_doc, RunConfig)
+
+    def retrieve_run_config_names(self):
+
+        return [f.name for f in self.runs.iterdir()]
+
+    def store_task_config(self, task_config):
+
+        task_doc = converter.unstructure(task_config)
+        self.__save_insert(self.tasks, task_doc)
+
+    def retrieve_task_config(self, task_name):
+
+        task_doc = self.__load(self.tasks, task_name)
+        return converter.structure(task_doc, TaskConfig)
+
+    def retrieve_task_config_names(self):
+
+        return [f.name for f in self.tasks.iterdir()]
+
+    def store_architecture_config(self, architecture_config):
+
+        architecture_doc = converter.unstructure(architecture_config)
+        self.__save_insert(self.architectures, architecture_doc)
+
+    def retrieve_architecture_config(self, architecture_name):
+
+        architecture_doc = self.__load(self.architectures, architecture_name)
+        return converter.structure(architecture_doc, ArchitectureConfig)
+
+    def retrieve_architecture_config_names(self):
+
+        architectures = self.architectures.find(
+            {}, projection={"_id": False, "name": True}
+        )
+        return list([architecture["name"] for architecture in architectures])
+
+    def store_trainer_config(self, trainer_config):
+
+        trainer_doc = converter.unstructure(trainer_config)
+        self.__save_insert(self.trainers, trainer_doc)
+
+    def retrieve_trainer_config(self, trainer_name):
+
+        trainer_doc = self.__load(self.trainers, trainer_name)
+        return converter.structure(trainer_doc, TrainerConfig)
+
+    def retrieve_trainer_config_names(self):
+
+        return [f.name for f in self.trainers.iterdir()]
+
+    def store_datasplit_config(self, datasplit_config):
+
+        datasplit_doc = converter.unstructure(datasplit_config)
+        self.__save_insert(self.datasplits, datasplit_doc)
+
+    def retrieve_datasplit_config(self, datasplit_name):
+
+        datasplit_doc = self.__load(self.datasplits, datasplit_name)
+        return converter.structure(datasplit_doc, DataSplitConfig)
+
+    def retrieve_datasplit_config_names(self):
+
+        return [f.name for f in self.datasplits.iterdir()]
+
+    def __save_insert(self, collection, data, ignore=None):
+
+        name = data["name"]
+
+        file_store = collection / name
+        if not file_store.exists():
+            pickle.dump(dict(data), file_store.open("wb"))
+
+        else:
+
+            existing = pickle.load(file_store.open("rb"))
+
+            if not self.__same_doc(existing, data, ignore):
+
+                raise DuplicateNameError(
+                    f"Data for {name} does not match already stored "
+                    f"entry. Found\n\n{existing}\n\nin DB, but was "
+                    f"given\n\n{data}"
+                )
+
+    def __load(self, collection, name):
+        file_store = collection / name
+        if file_store.exists():
+            return pickle.load(file_store.open("rb"))
+        else:
+            raise ValueError(f"No config with name: {name} in collection: {collection}")
+
+    def __same_doc(self, a, b, ignore=None):
+
+        if ignore:
+            a = dict(a)
+            b = dict(b)
+            for key in ignore:
+                if key in a:
+                    del a[key]
+                if key in b:
+                    del b[key]
+
+        return a == b
+
+    def __init_db(self):
+        # no indexing for filesystem
+        # please only use this config store for debugging
+        pass
+
+    def __open_collections(self):
+
+        self.users = self.path / "users"
+        self.users.mkdir(exist_ok=True)
+        self.runs = self.path / "runs"
+        self.runs.mkdir(exist_ok=True)
+        self.tasks = self.path / "tasks"
+        self.tasks.mkdir(exist_ok=True)
+        self.datasplits = self.path / "datasplits"
+        self.datasplits.mkdir(exist_ok=True)
+        self.architectures = self.path / "architectures"
+        self.architectures.mkdir(exist_ok=True)
+        self.trainers = self.path / "trainers"
+        self.trainers.mkdir(exist_ok=True)
---
+++ dacapo/store/file_stats_store.py
@@ -0,0 +1,179 @@
+from .stats_store import StatsStore
+from pymongo import MongoClient, ASCENDING
+from .converter import converter
+from dacapo.experiments import TrainingStats, TrainingIterationStats
+from dacapo.experiments import ValidationScores, ValidationIterationScores
+from typing import List
+
+import logging
+import pickle
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+class FileStatsStore(StatsStore):
+    """A File based store for run statistics. Used to store and retrieve training
+    statistics and validation scores.
+    """
+
+    def __init__(self, path):
+
+        logger.info(
+            "Creating MongoStatsStore:\n\tpath    : %s",
+            path)
+
+        self.path = Path(path)
+
+        self.__open_collections()
+        self.__init_db()
+
+    def store_training_stats(self, run_name, stats):
+
+        existing_stats = self.__read_training_stats(run_name)
+
+        store_from_iteration = 0
+
+        if existing_stats.trained_until() > 0:
+
+            if stats.trained_until() > 0:
+
+                # both current stats and DB contain data
+                if stats.trained_until() > existing_stats.trained_until():
+                    # current stats go further than the one in DB
+                    store_from_iteration = existing_stats.trained_until()
+                    logger.info(
+                        "Updating training stats of run %s after iteration %d",
+                        run_name,
+                        store_from_iteration)
+                else:
+                    # current stats are behind DB--drop DB
+                    logger.warn(
+                        "Overwriting previous training stats for run %s",
+                        run_name)
+                    self.__delete_training_stats(run_name)
+
+        # store all new stats
+        self.__store_training_stats(
+            stats,
+            store_from_iteration,
+            stats.trained_until(),
+            run_name)
+
+    def retrieve_training_stats(self, run_name):
+
+        return self.__read_training_stats(run_name)
+
+    def store_validation_scores(self, run_name, scores):
+
+        existing_scores = self.__read_validation_scores(run_name)
+        existing_scores_until = existing_scores.validated_until()
+
+        store_from_iteration = 0
+
+        if existing_scores_until > 0:
+
+            if scores.validated_until() > 0:
+
+                # both current scores and DB contain data
+                if scores.validated_until() > existing_scores_until:
+                    # current scores go further than the one in DB
+                    store_from_iteration = existing_scores_until
+                    logger.info(
+                        "Updating validation scores of run %s after iteration "
+                        "%d",
+                        run_name,
+                        store_from_iteration)
+                else:
+                    # current scores are behind DB--drop DB
+                    logger.warn(
+                        "Overwriting previous validation scores for run %s",
+                        run_name)
+                    self.__delete_validation_scores(run_name)
+
+        self.__store_validation_scores(
+            scores,
+            store_from_iteration,
+            scores.validated_until() + 1,
+            run_name)
+
+    def retrieve_validation_scores(self, run_name):
+
+        return self.__read_validation_scores(run_name)
+
+    def __store_training_stats(self, stats, begin, end, run_name):
+
+        docs = converter.unstructure(stats.iteration_stats[begin:end])
+        for doc in docs:
+            doc.update({'run_name': run_name})
+
+        if docs:
+            file_store = self.training_stats / run_name
+            pickle.dump(docs, file_store.open("wb"))
+
+    def __read_training_stats(self, run_name):
+
+        file_store = self.training_stats / run_name
+        if file_store.exists():
+            docs = pickle.load(file_store.open("rb"))
+        else:
+            docs = []
+        stats = TrainingStats(
+            converter.structure(
+                docs,
+                List[TrainingIterationStats]))
+        return stats
+
+    def __delete_training_stats(self, run_name):
+
+        file_store = self.training_stats / run_name
+        if file_store.exists():
+            file_store.unlink()
+
+    def __store_validation_scores(
+            self,
+            validation_scores,
+            begin,
+            end,
+            run_name):
+
+        docs = [
+            converter.unstructure(scores)
+            for scores in validation_scores.iteration_scores
+            if scores.iteration >= begin and scores.iteration < end
+        ]
+        for doc in docs:
+            doc.update({'run_name': run_name})
+
+        if docs:
+            file_store = self.validation_scores / run_name
+            pickle.dump(docs, file_store.open("wb"))
+
+    def __read_validation_scores(self, run_name):
+
+        file_store = self.validation_scores / run_name
+        if file_store.exists():
+            docs = pickle.load(file_store.open("rb"))
+        else:
+            docs = []
+        scores = ValidationScores(
+            converter.structure(
+                docs,
+                List[ValidationIterationScores]))
+        return scores
+
+    def __delete_validation_scores(self, run_name):
+
+        file_store = self.validation_scores / run_name
+        if file_store.exists():
+            file_store.unlink()
+
+    def __init_db(self):
+        pass
+
+    def __open_collections(self):
+
+        self.training_stats = self.path / 'training_stats'
+        self.training_stats.mkdir(exist_ok=True)
+        self.validation_scores = self.path / 'validation_scores'
+        self.validation_scores.mkdir(exist_ok=True)
