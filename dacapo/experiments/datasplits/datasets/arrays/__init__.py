60ece3eb33d68d166fd58124a7659f2db9df7ea8
pattonw <pattonw@hhmi.org>
2021-10-27 16:26:34 -0400

    added cellmap array/config to arrays __init__.py

--- dacapo/experiments/datasplits/datasets/arrays/__init__.py
+++ dacapo/experiments/datasplits/datasets/arrays/__init__.py
@@ -4,3 +4,5 @@ from .dummy_array import DummyArray  # noqa
 from .dummy_array_config import DummyArrayConfig  # noqa
 from .zarr_array import ZarrArray  # noqa
 from .zarr_array_config import ZarrArrayConfig  # noqa
+from .cellmap_array import CellMapArray  # noqa
+from .cellmap_array_config import CellMapArrayConfig  # noqa
