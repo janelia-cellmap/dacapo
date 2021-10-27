e0f7c039c018f48e71fd4f2eacf3edfc1948fde2
pattonw <pattonw@hhmi.org>
2021-10-27 16:26:12 -0400

    added a cellmap array to the tests

--- tests/fixtures/arrays/__init__.py
+++ tests/fixtures/arrays/__init__.py
@@ -1,4 +1,5 @@
 from .dummy_array import mk_dummy_array
 from .zarr_array import mk_zarr_array
+from .cellmap_array import mk_cellmap_array
 
-ARRAY_MK_FUNCTIONS = [mk_dummy_array, mk_zarr_array]
+ARRAY_MK_FUNCTIONS = [mk_dummy_array, mk_zarr_array, mk_cellmap_array]
---
+++ tests/fixtures/arrays/cellmap_array.py
@@ -0,0 +1,27 @@
+from dacapo.experiments.datasplits.datasets.arrays import ZarrArrayConfig, CellMapArrayConfig
+
+import zarr
+import numpy as np
+
+
+def mk_cellmap_array(temp_path):
+    zarr_array_config = ZarrArrayConfig(
+        name="zarr_array",
+        file_name=temp_path / "zarr_array.zarr",
+        dataset="volumes/test",
+    )
+    zarr_container = zarr.open(str(temp_path / "zarr_array.zarr"))
+    dataset = zarr_container.create_dataset(
+        "volumes/test", data=np.arange(0,100).reshape(10,5,2)
+    )
+    dataset.attrs["offset"] = (12,12,12)
+    dataset.attrs["resolution"] = (1, 2, 4)
+    dataset.attrs["axes"] = "zyx"
+
+    cellmap_array_config = CellMapArrayConfig(
+        name="cellmap_zarr_array",
+        source_array_config=zarr_array_config,
+        groupings=[list(range(0,10)), list(range(10,70)), list(range(70,90))]
+    )
+
+    return cellmap_array_config
