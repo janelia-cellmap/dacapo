f407056709b5886d66772ba6a3baeb69527dc7ad
pattonw <pattonw@hhmi.org>
2021-11-04 16:51:12 -0400

    update trainer test to test context and iteration

--- tests/components/test_trainers.py
+++ tests/components/test_trainers.py
@@ -10,24 +10,46 @@ from funlib.geometry import Coordinate, Roi
 
 import pytest
 
+NUM_ITER = 10
+
 
 @pytest.mark.parametrize("architecture_config", ARCHITECTURE_CONFIGS)
 @pytest.mark.parametrize("datasplit_mk_function", DATASPLIT_MK_FUNCTIONS)
 @pytest.mark.parametrize("task_config", TASK_CONFIGS)
 @pytest.mark.parametrize("trainer_config", TRAINER_CONFIGS)
-def test_arrays(
+def test_trainer(
     tmp_path,
     architecture_config,
     datasplit_mk_function,
     task_config,
     trainer_config,
 ):
+    # Initialize the config store
+    Options.instance(type="files", runs_base_dir=f"{tmp_path}")
+    store = create_config_store()
 
+    # Test store/retrieve
+    store.store_trainer_config(trainer_config)
+    fetched_array_config = store.retrieve_trainer_config(trainer_config.name)
+    assert fetched_array_config == trainer_config
+
+    # Create Trainer from config
+    trainer = trainer_config.trainer_type(trainer_config)
+
+    # Build prerequisites:
+    # Trainer can only train with data and a model:
     datasplit_config = datasplit_mk_function(tmp_path)
-
     architecture = architecture_config.architecture_type(architecture_config)
     datasplit = datasplit_config.datasplit_type(datasplit_config)
     task = task_config.task_type(task_config)
-    trainer = trainer_config.trainer_type(trainer_config)
 
-    trainer.build_batch_provider(datasplit, architecture, task)
+    model = task.predictor.create_model(architecture)
+    optimizer = trainer.create_optimizer(model)
+
+    # Trainer must build its batch provider:
+    trainer.build_batch_provider(datasplit.train, architecture, task)
+
+    # enter the training context:
+    with trainer as trainer:
+        training_stats = list(trainer.iterate(NUM_ITER, model, optimizer))
+        assert len(training_stats) == NUM_ITER
