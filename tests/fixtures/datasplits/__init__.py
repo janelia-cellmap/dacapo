3a951db1d600a69c4b83f5e994d6a89b3563f00e
pattonw <pattonw@hhmi.org>
2021-11-10 15:36:16 -0500

    make datasplit name more informative

--- tests/fixtures/datasplits/__init__.py
+++ tests/fixtures/datasplits/__init__.py
@@ -1,4 +1,4 @@
 from .dummy_datasplit import mk_dummy_datasplit
-from .trainable_datasplit import mk_trainable_datasplit
+from .twelve_class_datasplit import mk_twelve_class_datasplit
 
-DATASPLIT_MK_FUNCTIONS = [mk_dummy_datasplit, mk_trainable_datasplit]
+DATASPLIT_MK_FUNCTIONS = [mk_dummy_datasplit, mk_twelve_class_datasplit]
--- tests/fixtures/datasplits/twelve_class_datasplit.py
+++ tests/fixtures/datasplits/twelve_class_datasplit.py
@@ -6,51 +6,52 @@ import zarr
 import numpy as np
 
 
-def mk_trainable_datasplit(tmp_path):
-    trainable_zarr = zarr.open(tmp_path / "trainable.zarr", "w")
+def mk_twelve_class_datasplit(tmp_path):
+    twelve_class_zarr = zarr.open(tmp_path / "twelve_class.zarr", "w")
     crop1_raw = ZarrArrayConfig(
         name="crop1_raw",
-        file_name=tmp_path / "trainable.zarr",
+        file_name=tmp_path / "twelve_class.zarr",
         dataset=f"volumes/crop1/raw",
     )
     crop1_gt = ZarrArrayConfig(
         name="crop1_gt",
-        file_name=tmp_path / "trainable.zarr",
+        file_name=tmp_path / "twelve_class.zarr",
         dataset=f"volumes/crop1/gt",
     )
     crop2_raw = ZarrArrayConfig(
         name="crop2_raw",
-        file_name=tmp_path / "trainable.zarr",
+        file_name=tmp_path / "twelve_class.zarr",
         dataset=f"volumes/crop2/raw",
     )
     crop2_gt = ZarrArrayConfig(
         name="crop2_gt",
-        file_name=tmp_path / "trainable.zarr",
+        file_name=tmp_path / "twelve_class.zarr",
         dataset=f"volumes/crop2/gt",
     )
     crop3_raw = ZarrArrayConfig(
         name="crop3_raw",
-        file_name=tmp_path / "trainable.zarr",
+        file_name=tmp_path / "twelve_class.zarr",
         dataset=f"volumes/crop3/raw",
     )
     crop3_gt = ZarrArrayConfig(
         name="crop3_gt",
-        file_name=tmp_path / "trainable.zarr",
+        file_name=tmp_path / "twelve_class.zarr",
         dataset=f"volumes/crop3/gt",
     )
     for raw, gt in zip(
         [crop1_raw, crop2_raw, crop3_raw], [crop1_gt, crop2_gt, crop3_gt]
     ):
-        raw_dataset = trainable_zarr.create_dataset(
+        raw_dataset = twelve_class_zarr.create_dataset(
             raw.dataset, shape=(100, 100, 100), dtype=np.float32
         )
-        gt_dataset = trainable_zarr.create_dataset(
+        gt_dataset = twelve_class_zarr.create_dataset(
             gt.dataset, shape=(100, 100, 100), dtype=np.uint8
         )
         random_data = np.random.randn(100, 100, 100)
-        mask = random_data > 0.9
+        # as intensities increase so does the class
+        for i in list(np.linspace(random_data.min(), random_data.max(), 13))[1:]:
+            gt_dataset[:] += random_data > i
         raw_dataset[:] = random_data
-        gt_dataset[:] = mask
         raw_dataset.attrs["offset"] = (0, 0, 0)
         raw_dataset.attrs["resolution"] = (2, 2, 2)
         gt_dataset.attrs["offset"] = (0, 0, 0)
@@ -60,9 +61,9 @@ def mk_trainable_datasplit(tmp_path):
     crop2 = RawGTDatasetConfig(name="crop2", raw_config=crop2_raw, gt_config=crop2_gt)
     crop3 = RawGTDatasetConfig(name="crop3", raw_config=crop3_raw, gt_config=crop3_gt)
 
-    trainable_datasplit_config = TrainValidateDataSplitConfig(
-        name="trainable_datasplit",
+    twelve_class_datasplit_config = TrainValidateDataSplitConfig(
+        name="twelve_class_datasplit",
         train_configs=[crop1, crop2],
         validate_configs=[crop3],
     )
-    return trainable_datasplit_config
+    return twelve_class_datasplit_config
