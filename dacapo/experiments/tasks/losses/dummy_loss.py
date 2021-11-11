3504bbc1e2237a62134042e18f6249e3c7a24674
pattonw <pattonw@hhmi.org>
2021-11-11 10:35:58 -0500

    Make DummyLoss return a scalar
    Torch cannot compute a gradient if loss is not a scalar

--- dacapo/experiments/tasks/losses/dummy_loss.py
+++ dacapo/experiments/tasks/losses/dummy_loss.py
@@ -1,7 +1,6 @@
 from .loss import Loss
 
-
 class DummyLoss(Loss):
 
     def compute(self, prediction, target, weight=None):
-        return abs(prediction - target)
+        return abs(prediction - target).sum()
