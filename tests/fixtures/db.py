6271b1ffa144b5b9d23ab9b0ac87cc003f555ce6
pattonw <pattonw@hhmi.org>
2021-11-12 12:43:01 -0500

    Add a fixture for options
    Options are used to access state storage in DaCapo. Since we have
    multiple options we want to test them all. Using the options fixture
    makes this easy.

---
+++ tests/fixtures/db.py
@@ -0,0 +1,43 @@
+from dacapo import Options
+
+import pymongo
+import pytest
+
+
+def mongo_db_available():
+    try:
+        options = Options.instance()
+        client = pymongo.MongoClient(host=options.mongo_db_host, serverSelectionTimeoutMS=1000)
+        Options._instance = None
+    except RuntimeError:
+        # cannot find a dacapo config file, mongodb is not available
+        Options._instance=None
+        return False
+    try:
+        client.admin.command("ping")
+        return True
+    except pymongo.errors.ConnectionFailure:
+        return False
+
+
+@pytest.fixture(
+    params=[
+        "files",
+        pytest.param("mongo", marks=pytest.mark.skipif(not mongo_db_available(), reason="MongoDB not available!")),
+    ]
+)
+def options(request, tmp_path):
+    kwargs_from_file = {}
+    if request.param == "mongo":
+        options_from_file = Options.instance()
+        kwargs_from_file.update({
+            "mongo_db_host": options_from_file.mongo_db_host,
+            "mongo_db_name": "dacapo_tests",
+        })
+    Options._instance = None
+    options = Options.instance(type=request.param, runs_base_dir=f"{tmp_path}", **kwargs_from_file)
+    yield options
+    if request.param == "mongo":
+        client = pymongo.MongoClient(host=options.mongo_db_host)
+        client.drop_database("dacapo_tests")
+    Options._instance = None
