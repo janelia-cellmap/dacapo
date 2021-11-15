4abc4ef0154b6f08be6b8421377b85adcabebfc9
pattonw <pattonw@hhmi.org>
2021-11-15 12:52:01 -0500

    git the dummy_datasplit a validation split
    Can't test validate on a DummyDataSplit unless it has validation data

--- dacapo/experiments/datasplits/dummy_datasplit.py
+++ dacapo/experiments/datasplits/dummy_datasplit.py
@@ -1,15 +1,21 @@
 from .datasplit import DataSplit
 from .datasets import Dataset
 
+from typing import List
+
 
 class DummyDataSplit(DataSplit):
 
-    train: Dataset = None
+    train: List[Dataset] = None
+    validate: List[Dataset] = None
 
     def __init__(self, datasplit_config):
 
         super().__init__()
 
         self.train = [
+            datasplit_config.train_config.datasplit_type(datasplit_config.train_config)
+        ]
+        self.validate = [
             datasplit_config.train_config.datasplit_type(datasplit_config.train_config)
         ]
