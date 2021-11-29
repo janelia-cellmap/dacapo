b22f9fd865865bccc44e138ca3ebc9d2957793ab
pattonw <pattonw@hhmi.org>
2021-11-29 16:21:58 -0500

    simplify training stats. They should always be in order

--- dacapo/experiments/training_stats.py
+++ dacapo/experiments/training_stats.py
@@ -6,19 +6,18 @@ import attr
 @attr.s
 class TrainingStats:
 
-    iteration_stats: List[TrainingIterationStats] = attr.ib(
-        default=attr.Factory(list))
+    iteration_stats: List[TrainingIterationStats] = attr.ib(default=attr.Factory(list))
 
     def add_iteration_stats(self, iteration_stats):
+        if len(self.iteration_stats) > 0:
+            assert iteration_stats.iteration == self.iteration_stats[-1].iteration + 1
 
         self.iteration_stats.append(iteration_stats)
 
     def delete_after(self, iteration):
 
         self.iteration_stats = [
-            stats
-            for stats in self.iteration_stats
-            if stats.iteration < iteration
+            stats for stats in self.iteration_stats if stats.iteration < iteration
         ]
 
     def trained_until(self):
@@ -27,4 +26,4 @@ class TrainingStats:
 
         if not self.iteration_stats:
             return 0
-        return max([stat.iteration for stat in self.iteration_stats]) + 1
+        return self.iteration_stats[-1].iteration + 1
