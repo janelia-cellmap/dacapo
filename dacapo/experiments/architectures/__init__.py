335713f02a6b47d5b917055514b647610b9aa9a2
pattonw <pattonw@hhmi.org>
2021-11-29 12:43:59 -0500

    add CNNectomeUNetConfig to __init__.py

--- dacapo/experiments/architectures/__init__.py
+++ dacapo/experiments/architectures/__init__.py
@@ -1,2 +1,3 @@
 from .architecture_config import ArchitectureConfig  # noqa
 from .dummy_architecture_config import DummyArchitectureConfig  # noqa
+from .cnnectome_unet_config import CNNectomeUNetConfig  # noqa
