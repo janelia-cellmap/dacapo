7e64da57dcdbc1b43623621991aedd8d1ae69b14
pattonw <pattonw@hhmi.org>
2021-11-30 11:04:17 -0500

    remove segmentation evaluator from __init__

--- dacapo/experiments/tasks/evaluators/__init__.py
+++ dacapo/experiments/tasks/evaluators/__init__.py
@@ -4,5 +4,3 @@ from .evaluation_scores import EvaluationScores  # noqa
 from .evaluator import Evaluator  # noqa
 from .binary_segmentation_evaluation_scores import MultiChannelBinarySegmentationEvaluationScores, BinarySegmentationEvaluationScores  # noqa
 from .binary_segmentation_evaluator import BinarySegmentationEvaluator  # noqa
-from .multi_class_segmentation_evaluation_scores import MultiClassSegmentationEvaluationScores  # noqa
-from .multi_class_segmentation_evaluator import MultiClassSegmentationEvaluator  # noqa
