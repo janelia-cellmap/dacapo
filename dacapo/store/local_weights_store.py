25ea15fcf30d69159d88d3ded02c018eb0eb96c6
pattonw <pattonw@hhmi.org>
2022-01-24 16:56:23 -0500

    retrieve best checkpoint according to criterion

--- dacapo/store/local_weights_store.py
+++ dacapo/store/local_weights_store.py
@@ -68,6 +68,18 @@ class LocalWeightsStore(WeightsStore):
 
         best_weights.write_bytes(iteration_weights.read_bytes())
 
+    def retrieve_best(self, run, criterion):
+
+        logger.info("Retrieving weights for run %s, criterion %s", run.name, criterion)
+
+        weights_name = self.__get_weights_dir(run) / criterion
+
+        weights = torch.load(weights_name, map_location="cpu")
+
+        run.model.load_state_dict(weights["model"])
+        run.optimizer.load_state_dict(weights["optimizer"])
+
+
     def retrieve_weights(self, run, iteration):
         """Retrieve the network weights of the given run."""
 
