adb1a53551943baa21f06f2d53e49ffd6851b9c5
pattonw <pattonw@hhmi.org>
2022-01-24 16:53:43 -0500

    add support for masking distances

--- dacapo/experiments/tasks/distance_task_config.py
+++ dacapo/experiments/tasks/distance_task_config.py
@@ -20,25 +20,25 @@ class DistanceTaskConfig(TaskConfig):
 
     task_type = DistanceTask
 
-    channels: List[str] = attr.ib(
-        metadata={
-            "help_text": "A list of channel names."
-        }
-    )
+    channels: List[str] = attr.ib(metadata={"help_text": "A list of channel names."})
     clip_distance: float = attr.ib(
-        metadata={
-            "help_text": ""
-        },
+        metadata={"help_text": ""},
     )
     tol_distance: float = attr.ib(
-        metadata={
-            "help_text": ""
-        },
+        metadata={"help_text": ""},
     )
     scale_factor: float = attr.ib(
         default=1,
         metadata={
             "help_text": "The amount by which to scale distances before applying "
             "a tanh normalization."
+        },
+    )
+    mask_distances: bool = attr.ib(
+        default=False,
+        metadata={
+            "help_text": "Whether or not to mask out regions where the true distance to "
+            "object boundary cannot be known. This is anywhere that the distance to crop boundary "
+            "is less than the distance to object boundary."
         },
     )
