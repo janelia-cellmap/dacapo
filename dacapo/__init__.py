be200070de7107fa4d2794b33aec497b7ed2e6a3
pattonw <pattonw@hhmi.org>
2022-01-25 16:36:07 -0500

    Options are expected to be used throughout dacapo
    having the imports lower leads to circular imports

--- dacapo/__init__.py
+++ dacapo/__init__.py
@@ -1,5 +1,5 @@
+from .options import Options  # noqa
 from . import experiments  # noqa
 from .apply import apply  # noqa
-from .options import Options  # noqa
 from .train import train  # noqa
 from .validate import validate  # noqa
