254389f8caf04be069f89a31e4b4d5ca33a9030b
pattonw <pattonw@hhmi.org>
2022-01-28 12:59:49 -0500

    implify validation scores
    structuring/destructuring deeply nested classes of classes seems to be
    excessively slow. simplifying this in the hope that we can generate
    plots much faster and easier.

--- dacapo/experiments/validation_iteration_scores.py
+++ dacapo/experiments/validation_iteration_scores.py
@@ -10,30 +10,8 @@ class ValidationIterationScores:
     iteration: int = attr.ib(
         metadata={"help_text": "The iteration associated with these validation scores."}
     )
-    parameter_scores: List[Tuple[PostProcessorParameters, EvaluationScores]] = attr.ib(
+    scores: List[List[float]] = attr.ib(
         metadata={
-            "help_text": "A list of evaluation scores and their associated post-processing parameters."
+            "help_text": "A list of scores per post processor parameters and evaluation criterion."
         }
     )
-    dataset: str = attr.ib(
-        default=None, metadata={"help_text": "The dataset that these stats belong to."}
-    )
-
-    @property
-    def scores(self):
-        return [score for params, score in self.parameter_scores]
-
-    @property
-    def criteria(self):
-        if self.parameter_scores:
-            return self.parameter_scores[0][1].criteria
-
-        raise RuntimeError("No scores to evaluate yet")
-
-    @property
-    def parameter_names(self):
-        if self.parameter_scores:
-            postprocessor_class_instance = self.parameter_scores[0][0]
-            return postprocessor_class_instance.parameter_names
-
-        raise RuntimeError("No scores to evaluate yet")
--- dacapo/experiments/validation_scores.py
+++ dacapo/experiments/validation_scores.py
@@ -1,15 +1,35 @@
+from importlib.metadata import metadata
 from .validation_iteration_scores import ValidationIterationScores
+from .tasks.evaluators import EvaluationScores
+from .tasks.post_processors import PostProcessorParameters
+from .datasplits.datasets import Dataset
+
 from typing import List
 import attr
 import numpy as np
 import inspect
 
+
 @attr.s
 class ValidationScores:
 
-    iteration_scores: List[ValidationIterationScores] = attr.ib(
-        default=attr.Factory(list),
-        metadata={"help_text": "An ordered list of validation scores by iteration."},
+    parameters: list[PostProcessorParameters] = attr.ib(
+        metadata={"help_text": "The list of parameters that are being evaluated"}
+    )
+    datasets: List[Dataset] = attr.ib(
+        metadata={"help_text": "The datasets that will be evaluated at each iteration"}
+    )
+    evaluation_scores: EvaluationScores = attr.ib(
+        metadata={
+            "help_text": "The scores that are collected on each iteration per "
+            "`PostProcessorParameters` and `Dataset`"
+        }
+    )
+    scores: List[ValidationIterationScores] = attr.ib(
+        factor=lambda: list(),
+        metadata={
+            "help_text": "A list of evaluation scores and their associated post-processing parameters."
+        },
     )
 
     def add_iteration_scores(self, iteration_scores):
@@ -19,9 +39,7 @@ class ValidationScores:
     def delete_after(self, iteration):
 
         self.iteration_scores = [
-            scores
-            for scores in self.iteration_scores
-            if scores.iteration < iteration
+            scores for scores in self.iteration_scores if scores.iteration < iteration
         ]
 
     def validated_until(self):
@@ -35,76 +53,59 @@ class ValidationScores:
     def get_attribute_names(self, class_instance):
 
         attributes = inspect.getmembers(
-            class_instance, lambda a: not(inspect.isroutine(a)))
-        names = [a[0] for a in attributes if not(
-            a[0].startswith('__') and a[0].endswith('__'))]
+            class_instance, lambda a: not (inspect.isroutine(a))
+        )
+        names = [
+            a[0]
+            for a in attributes
+            if not (a[0].startswith("__") and a[0].endswith("__"))
+        ]
 
         return names
 
     @property
     def criteria(self):
-        if self.iteration_scores:
-            return self.iteration_scores[0].criteria
-        
-        raise RuntimeError("No scores to evaluate yet")
+        return self.evaluation_scores.criteria
 
     @property
     def parameter_names(self):
-        if self.iteration_scores:
-            return self.iteration_scores[0].parameter_names
-        
-        raise RuntimeError("No scores to evaluate yet")
+        return self.parameters[0].parameter_names
 
-    def get_score_names(self):
-
-        if self.iteration_scores:
-            example_parameter_scores = self.iteration_scores[0].parameter_scores
-            score_class_instance = example_parameter_scores[0][1]
-            return score_class_instance.criteria
-
-        raise RuntimeError("No scores were added, yet")
-
-    def get_postprocessor_parameter_names(self):
-
-        if self.iteration_scores:
-            return self.iteration_scores[0].parameter_names
-
-        raise RuntimeError("No scores were added, yet")
-
-    def get_best(self, score_name=None, higher_is_better=True):
+    def get_best(self, criterion=None, higher_is_better=True):
+        """
+        return the best score according to this criterion
+        """
 
         names = self.get_score_names()
         postprocessor_parameter_names = self.get_postprocessor_parameter_names()
 
         best_scores = {name: [] for name in names}
-        best_score_parameters = {name: []
-                                 for name in postprocessor_parameter_names}
+        best_score_parameters = {name: [] for name in postprocessor_parameter_names}
 
         for iteration_score in self.iteration_scores:
-            ips = np.array([
-                getattr(parameter_score[1], score_name, np.nan)
-                for parameter_score in iteration_score.parameter_scores
-            ], dtype=np.float32)
+            ips = np.array(
+                [
+                    getattr(parameter_score[1], criterion, np.nan)
+                    for parameter_score in iteration_score.parameter_scores
+                ],
+                dtype=np.float32,
+            )
             ips[np.isnan(ips)] = -np.inf if higher_is_better else np.inf
             i = np.argmax(ips) if higher_is_better else np.argmin(ips)
             best_score = iteration_score.parameter_scores[i]
 
             for name in names:
                 try:
-                    best_scores[name].append(
-                        getattr(best_score[1], name)
-                    )
+                    best_scores[name].append(getattr(best_score[1], name))
                 except AttributeError as e:
                     raise AttributeError(iteration_score.iteration) from e
 
             for name in postprocessor_parameter_names:
-                best_score_parameters[name].append(
-                    getattr(best_score[0], name)
-                )
+                best_score_parameters[name].append(getattr(best_score[0], name))
 
         return (best_score_parameters, best_scores)
 
-    def _get_best(self, criterion):
+    def _get_best(self, criterion, dataset=None):
         """
         Get the best score according to this criterion.
         return iteration, parameters, score
@@ -112,34 +113,7 @@ class ValidationScores:
         iteration_bests = []
         for iteration_score in self.iteration_scores:
             parameters, iteration_best = iteration_score._get_best(criterion)
-            iteration_bests.append((iteration_score.iteration, parameters, iteration_best))
+            iteration_bests.append(
+                (iteration_score.iteration, parameters, iteration_best)
+            )
 
-        
-
-'''
-    def get_score_names(self):
-
-        for scores in self.scores:
-            for parameters, sample_scores in scores.items():
-                return sample_scores['scores']['average'].keys()
-
-        raise RuntimeError("No scores were added, yet")
-
-    def get_best(self, score_name=None, higher_is_better=True):
-
-        names = self.get_score_names()
-
-        best_scores = {name: [] for name in names}
-        for iteration_scores in self.scores:
-            ips = np.array([
-                parameter_scores['scores']['average'].get(score_name, np.nan)
-                for parameter_scores in iteration_scores.values()
-            ], dtype=np.float32)
-            ips[np.isnan(ips)] = -np.inf if higher_is_better else np.inf
-            i = np.argmax(ips) if higher_is_better else np.argmin(ips)
-            for name in names:
-                best_scores[name].append(
-                    list(iteration_scores.values())[i]['scores']['average'].get(name, 0)
-                )
-        return best_scores
-'''
