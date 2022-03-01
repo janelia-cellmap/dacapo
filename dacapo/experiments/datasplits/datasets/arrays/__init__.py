9b53daed5fe4292fac91da39f0d9c90aebc3c178
pattonw <pattonw@hhmi.org>
2022-03-01 17:17:24 -0500

    add OnesArray and add comments

--- dacapo/experiments/datasplits/datasets/arrays/__init__.py
+++ dacapo/experiments/datasplits/datasets/arrays/__init__.py
@@ -1,13 +1,20 @@
 from .array import Array  # noqa
 from .array_config import ArrayConfig  # noqa
+
+# configurable arrays
 from .dummy_array import DummyArray  # noqa
 from .dummy_array_config import DummyArrayConfig  # noqa
 from .zarr_array import ZarrArray  # noqa
 from .zarr_array_config import ZarrArrayConfig  # noqa
 from .binarize_array import BinarizeArray  # noqa
 from .binarize_array_config import BinarizeArrayConfig  # noqa
-from .numpy_array import NumpyArray  # noqa
 from .resampled_array import ResampledArray  # noqa
 from .resampled_array_config import ResampledArrayConfig  # noqa
 from .intensity_array import IntensitiesArray  # noqa
 from .intensity_array_config import IntensitiesArrayConfig  # noqa
+from .missing_annotations_mask import MissingAnnotationsMask  # noqa
+from .missing_annotations_mask_config import MissingAnnotationsMaskConfig  # noqa
+
+# nonconfigurable arrays (helpers)
+from .numpy_array import NumpyArray  # noqa
+from .ones_array import OnesArray  # noqa
---
+++ dacapo/experiments/datasplits/datasets/arrays/ones_array.py
@@ -0,0 +1,63 @@
+from .array import Array
+
+import gunpowder as gp
+from funlib.geometry import Coordinate, Roi, roi
+
+import numpy as np
+
+
+class OnesArray(Array):
+    """This is a wrapper around another `source_array` that simply provides ones
+    with the same metadata as the `source_array`."""
+
+    def __init__(self, array_config):
+        raise RuntimeError("Ones Array cannot be built from a config file")
+
+    @classmethod
+    def like(cls, array: Array):
+        instance = cls.__new__(cls)
+        instance._source_array = array
+        return instance
+
+    @property
+    def attrs(self):
+        return dict()
+
+    @property
+    def source_array(self) -> Array:
+        return self._source_array
+
+    @property
+    def axes(self):
+        return self.source_array.axes
+
+    @property
+    def dims(self):
+        return self.source_array.dims
+
+    @property
+    def voxel_size(self):
+        return self.source_array.voxel_size
+
+    @property
+    def roi(self):
+        return self.source_array.roi
+
+    @property
+    def writable(self) -> bool:
+        return False
+
+    @property
+    def data(self):
+        raise RuntimeError("Cannot get writable version of this data!")
+
+    @property
+    def dtype(self):
+        return np.bool
+
+    @property
+    def num_channels(self):
+        return self.source_array.num_channels
+
+    def __getitem__(self, roi: Roi) -> np.ndarray:
+        return np.ones_like(self.source_array.__getitem__(roi), dtype=np.bool)
