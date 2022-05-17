c236854f1a1ec1a44fddf277aa7ee2f4354b50cd
pattonw <pattonw@hhmi.org>
2022-05-17 09:43:33 -0700

    improve documentation for TrainingStats

--- dacapo/experiments/training_stats.py
+++ dacapo/experiments/training_stats.py
@@ -15,27 +15,30 @@ class TrainingStats:
         metadata={"help_text": "A ordered list of training stats."},
     )
 
-    def add_iteration_stats(self, iteration_stats):
+    def add_iteration_stats(self, iteration_stats: TrainingIterationStats) -> None:
         if len(self.iteration_stats) > 0:
             assert iteration_stats.iteration == self.iteration_stats[-1].iteration + 1
 
         self.iteration_stats.append(iteration_stats)
 
-    def delete_after(self, iteration):
+    def delete_after(self, iteration: int) -> None:
 
         self.iteration_stats = [
             stats for stats in self.iteration_stats if stats.iteration < iteration
         ]
 
-    def trained_until(self):
-        """The number of iterations trained for (the maximum iteration plus
-        one)."""
+    def trained_until(self) -> int:
+        """
+        The number of iterations trained for (the maximum iteration plus
+        one).
+        0 if no iterations trained yet.
+        """
 
         if not self.iteration_stats:
             return 0
         return self.iteration_stats[-1].iteration + 1
 
-    def to_xarray(self):
+    def to_xarray(self) -> xr.DataArray:
         return xr.DataArray(
             np.array(
                 [iteration_stat.loss for iteration_stat in self.iteration_stats]
