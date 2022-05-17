9a6736e427b116df673d0383316e46405053e598
pattonw <pattonw@hhmi.org>
2022-05-17 15:34:34 -0700

    fix depricated np.bool warning

--- dacapo/experiments/tasks/predictors/distance_predictor.py
+++ dacapo/experiments/tasks/predictors/distance_predictor.py
@@ -213,7 +213,7 @@ class DistancePredictor(Predictor):
         out_shape = tuple(2 * s - 1 for s in in_shape)
         out_slices = tuple(slice(0, s) for s in out_shape)
 
-        boundaries = np.zeros(out_shape, dtype=np.bool)
+        boundaries = np.zeros(out_shape, dtype=bool)
 
         logger.debug("boundaries shape is %s", boundaries.shape)
 
