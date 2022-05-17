cb290f3b6f41a920fbeeadf3060c534338dd4e28
pattonw <pattonw@hhmi.org>
2022-05-17 09:43:19 -0700

    improve documentation for Run

--- dacapo/experiments/run.py
+++ dacapo/experiments/run.py
@@ -11,24 +11,29 @@ class Run:
 
         self.name = run_config.name
 
+        # config types
         task_type = run_config.task_config.task_type
         architecture_type = run_config.architecture_config.architecture_type
         trainer_type = run_config.trainer_config.trainer_type
         datasplit_type = run_config.datasplit_config.datasplit_type
 
+        # run components
         self.task = task_type(run_config.task_config)
         self.architecture = architecture_type(run_config.architecture_config)
         self.trainer = trainer_type(run_config.trainer_config)
         self.datasplit = datasplit_type(run_config.datasplit_config)
 
+        # combined pieces
         self.model = self.task.create_model(self.architecture)
         self.optimizer = self.trainer.create_optimizer(self.model)
 
+        # tracking
         self.training_stats = TrainingStats()
         self.validation_scores = ValidationScores(
             self.task.parameters, self.datasplit.validate, self.task.evaluation_scores
         )
 
+        # preloaded weights from previous run
         self.start = (
             Start(run_config.start_config)
             if run_config.start_config is not None
@@ -38,7 +43,10 @@ class Run:
             self.start.initialize_weights(self.model)
 
     @staticmethod
-    def get_validation_scores(run_config):
+    def get_validation_scores(run_config) -> None:
+        """
+        Static method to avoid having to initialize model, optimizer, trainer, etc.
+        """
         task_type = run_config.task_config.task_type
         datasplit_type = run_config.datasplit_config.datasplit_type
 
@@ -49,7 +57,7 @@ class Run:
             task.parameters, datasplit.validate, task.evaluation_scores
         )
 
-    def move_optimizer(self, device, empty_cuda_cache=False):
+    def move_optimizer(self, device: torch.Device, empty_cuda_cache: bool = False) -> None:
         for state in self.optimizer.state.values():
             for k, v in state.items():
                 if torch.is_tensor(v):
