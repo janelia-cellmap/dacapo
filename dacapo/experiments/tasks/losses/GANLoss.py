587a92e8d403cda60371483421af54c899f3a4ab
brianreicher <reicher.b@northeastern.edu>
2022-12-05 11:04:32 -0500

    GAN loss build

---
+++ dacapo/experiments/tasks/losses/GANLoss.py
@@ -0,0 +1,17 @@
+from .loss import Loss
+import torch
+
+
+class GANLoss(Loss):
+    def __init__(self, gan_mode):
+        self.gan_mode = gan_mode
+
+    def compute(self, prediction, target, weight):            
+        match self.gan_mode: 
+            case 'lsgan':
+                return torch.nn.MSELoss().forward(prediction * weight, target * weight)
+            case 'vanilla':
+                return torch.nn.BCEWithLogitsLoss().forward(prediction * weight, target * weight)
+
+        if self.gan_mode  in ['wgangp']:
+            return None
--- dacapo/experiments/tasks/losses/linked_cycle_loss.py
+++
@@ -1,8 +0,0 @@
-from .loss import Loss
-import torch
-
-
-class LinkedCycleLoss(Loss):
-    def compute(self, prediction, target, weight):
-        pass
-        # return torch.nn.MSELoss().forward(prediction * weight, target * weight)
