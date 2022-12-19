7b879a2a803827680abca192f94b9bf822e47945
pattonw <pattonw@hhmi.org>
2022-12-19 10:09:05 -0500

    compute sum over multiple array sources

---
+++ dacapo/experiments/datasplits/datasets/arrays/sum_array_config.py
@@ -0,0 +1,17 @@
+import attr
+
+from .array_config import ArrayConfig
+from .sum_array import SumArray
+from .array_config import ArrayConfig
+
+from typing import List
+
+
+@attr.s
+class SumArrayConfig(ArrayConfig):
+
+    array_type = SumArray
+
+    source_array_configs: List[ArrayConfig] = attr.ib(
+        metadata={"help_text": "The Array of masks from which to take the union"}
+    )
