791f08577819b50a7762ddee533a071343362b16
pattonw <pattonw@hhmi.org>
2023-05-10 00:18:02 -0400

    add a product node

--- dacapo/gp/__init__.py
+++ dacapo/gp/__init__.py
@@ -5,3 +5,4 @@ from .elastic_augment_fuse import ElasticAugment
 from .reject_if_empty import RejectIfEmpty
 from .copy import CopyMask
 from .dacapo_points_source import GraphSource
+from .product import Product
