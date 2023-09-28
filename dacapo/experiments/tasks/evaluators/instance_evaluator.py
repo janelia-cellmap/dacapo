d8c8517e5854c27ebdcc6374630a1597adca054e
rhoadesScholar <rhoadesj@hhmi.org>
2023-09-28 17:07:08 -0400

    feat: avg_iou

--- dacapo/experiments/tasks/evaluators/instance_evaluator.py
+++ dacapo/experiments/tasks/evaluators/instance_evaluator.py
@@ -5,6 +5,13 @@ from .instance_evaluation_scores import InstanceEvaluationScores
 
 from funlib.evaluate import rand_voi, detection_scores
 
+try:
+    from funlib.segment.arrays import relabel
+
+    iou = True
+except ImportError:
+    iou = False
+
 import numpy as np
 
 
@@ -16,18 +23,22 @@ class InstanceEvaluator(Evaluator):
         evaluation_data = evaluation_array[evaluation_array.roi].astype(np.uint64)
         output_data = output_array[output_array.roi].astype(np.uint64)
         results = rand_voi(evaluation_data, output_data)
-        # results.update(
-        #     detection_scores(
-        #         evaluation_data,
-        #         output_data,
-        #         matching_score="iou",
-        #     )
-        # )
+        if iou:
+            output_data, _ = relabel(output_data)
+            results.update(
+                detection_scores(
+                    evaluation_data,
+                    output_data,
+                    matching_score="iou",
+                )
+            )
+        else:
+            results["avg_iou"] = 0
 
         return InstanceEvaluationScores(
             voi_merge=results["voi_merge"],
             voi_split=results["voi_split"],
-            avg_iou=0,  # results["avg_iou"],
+            avg_iou=results["avg_iou"],
         )
 
     @property
