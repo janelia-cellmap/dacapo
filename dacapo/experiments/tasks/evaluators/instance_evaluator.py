2a29afdf940f4c33ac5731f991f842314f9aa294
rhoadesScholar <rhoadesj@hhmi.org>
2024-02-08 16:24:17 -0500

    feat: ⚡️ Incorporaten validation related changes from rhoadesj/dev

--- dacapo/experiments/tasks/evaluators/instance_evaluator.py
+++ dacapo/experiments/tasks/evaluators/instance_evaluator.py
@@ -3,22 +3,48 @@ from dacapo.experiments.datasplits.datasets.arrays import ZarrArray
 from .evaluator import Evaluator
 from .instance_evaluation_scores import InstanceEvaluationScores
 
-from funlib.evaluate import rand_voi
+from funlib.evaluate import rand_voi, detection_scores
+
+try:
+    from funlib.segment.arrays import relabel
+
+    iou = True
+except ImportError:
+    iou = False
 
 import numpy as np
 
 
 class InstanceEvaluator(Evaluator):
-    criteria = ["voi_merge", "voi_split", "voi"]
+    criteria = ["voi_merge", "voi_split", "voi", "avg_iou"]
 
     def evaluate(self, output_array_identifier, evaluation_array):
         output_array = ZarrArray.open_from_array_identifier(output_array_identifier)
         evaluation_data = evaluation_array[evaluation_array.roi].astype(np.uint64)
         output_data = output_array[output_array.roi].astype(np.uint64)
         results = rand_voi(evaluation_data, output_data)
+        if iou:
+            try:
+                output_data, _ = relabel(output_data)
+                results.update(
+                    detection_scores(
+                        evaluation_data,
+                        output_data,
+                        matching_score="iou",
+                    )
+                )
+            except Exception:
+                results["avg_iou"] = 0
+                logger.warning(
+                    "Could not compute IoU because of an unknown error. Sorry about that."
+                )
+        else:
+            results["avg_iou"] = 0
 
         return InstanceEvaluationScores(
-            voi_merge=results["voi_merge"], voi_split=results["voi_split"]
+            voi_merge=results["voi_merge"],
+            voi_split=results["voi_split"],
+            avg_iou=results["avg_iou"],
         )
 
     @property
