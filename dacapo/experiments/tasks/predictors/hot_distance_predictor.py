3fa459dfc3c149a5c1721732045700145105c52e
rhoadesScholar <rhoadesScholar@users.noreply.github.com>
2024-02-11 19:00:38 +0000

    :art: Format Python code with psf/black

--- dacapo/experiments/tasks/predictors/hot_distance_predictor.py
+++ dacapo/experiments/tasks/predictors/hot_distance_predictor.py
@@ -76,7 +76,9 @@ class HotDistancePredictor(Predictor):
             2,
             slab=tuple(1 if c == "c" else -1 for c in gt.axes),
             masks=[mask[target.roi]],
-            moving_counts=None if moving_class_counts is None else moving_class_counts[: self.classes],
+            moving_counts=None
+            if moving_class_counts is None
+            else moving_class_counts[: self.classes],
         )
 
         if self.mask_distances:
@@ -95,7 +97,9 @@ class HotDistancePredictor(Predictor):
             2,
             slab=tuple(1 if c == "c" else -1 for c in gt.axes),
             masks=[mask[target.roi], distance_mask],
-            moving_counts=None if moving_class_counts is None else moving_class_counts[-self.classes :],
+            moving_counts=None
+            if moving_class_counts is None
+            else moving_class_counts[-self.classes :],
         )
 
         weights = np.concatenate((one_hot_weights, distance_weights))
