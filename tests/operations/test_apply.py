8cbd3d2c8832d6fdf9320df3b4d502f58a497cab
rhoadesScholar <rhoadesj@hhmi.org>
2024-02-12 17:35:40 -0500

    test: 🚧 Add apply.py test

---
+++ tests/operations/test_apply.py
@@ -0,0 +1,54 @@
+from ..fixtures import *
+
+from dacapo.experiments import Run
+from dacapo.compute_context import LocalTorch
+from dacapo.store import create_config_store, create_weights_store
+from dacapo import apply
+
+import pytest
+from pytest_lazyfixture import lazy_fixture
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+@pytest.mark.parametrize(
+    "run_config",
+    [
+        lazy_fixture("distance_run"),
+        lazy_fixture("dummy_run"),
+        lazy_fixture("onehot_run"),
+    ],
+)
+def test_apply(
+    options,
+    run_config,
+):
+    compute_context = LocalTorch(device="cpu")
+
+    # create a store
+
+    store = create_config_store()
+    weights_store = create_weights_store()
+
+    # store the configs
+
+    store.store_run_config(run_config)
+
+    run_config = store.retrieve_run_config(run_config.name)
+    run = Run(run_config)
+
+    # -------------------------------------
+
+    # apply
+
+    # test validating iterations for which we know there are weights
+    weights_store.store_weights(run, 0)
+    apply(run_config.name, 0, compute_context=compute_context)  # TODO
+    weights_store.store_weights(run, 1)
+    apply(run_config.name, 1, compute_context=compute_context)
+
+    # test validating weights that don't exist
+    with pytest.raises(FileNotFoundError):
+        apply(run_config.name, 2, compute_context=compute_context)
