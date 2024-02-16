f7233beeb47322a771bb558e66ed30d522abc6d7
mzouink <zouinkhi.marwan@gmail.com>
2024-02-16 14:20:29 -0500

    cellpose unet in progress

---
+++ dacapo/experiments/architectures/cellpose_unet.py
@@ -0,0 +1,10 @@
+from cellpose.resnet_torch import CPnet
+
+class CellposeUnet(Architecture):
+    nbase = [in_chan, 32, 64, 128, 256]
+    nout = 4
+    sz  = 3
+    self.net = CPnet(nbase, nout, sz, mkldnn=False,
+           conv_3D=True, max_pool=True,
+            diam_mean=30.)
+    
