c47f1ae2ea854e0ab23a1a6821ab886ab39b173f
mzouink <zouinkhi.marwan@gmail.com>
2024-02-16 16:03:41 -0500

    remove blockwise non finish part

--- dacapo/blockwise/scheduler.py
+++ dacapo/blockwise/scheduler.py
@@ -1,79 +1,152 @@
 from pathlib import Path
+import tempfile
+import time
 import daisy
-from funlib.geometry import BoundingBox
+from funlib.geometry import Roi, Coordinate
+import yaml
 
-from dacapo.context import ComputeContext
-from dacapo.tasks import BlockwiseTask
+from dacapo.compute_context import ComputeContext
+from dacapo.blockwise import DaCapoBlockwiseTask
+
 
 def run_blockwise(
-  worker_file: str | Path,
-  context: ComputeContext | str,
-  total_box: BoundingBox,
-  read_box: BoundingBox,
-  write_box: BoundingBox,
-  num_workers: int = 16,
-  max_attempts: int = 2,
-  timeout=None,
-  dependencies=None,
-  *args,
-  **kwargs,
-):
-  """
-  Coordinate a blockwise computation over a large volume.
-
-  Args:
-    worker_file (str or Path): The path to a Python file which defines the 
-    method to be run, the process to spawn workers, and the check to be
-    applied after each worker's computation.
-    
-    context (ComputeContext or str): The context to use for computation.
-    May either be a ComputeContext instance or a string from which a context
-    can be derived.
-    
-    total_box (BoundingBox): The total bounding box over which to cover 
-    with computations.
-    
-    read_box (BoundingBox): The bounding box for which each worker must 
-    read data. This box will be translated across the total_box for each 
-    worker.
-    
-    write_box (BoundingBox): The bounding box within which each worker will 
-    write data. This box will be translated across the total_box for each 
-    worker.
-    
-    num_workers (int, optional): The number of workers to accommodate.
-    Defaults to 16.
-    
-    max_attempts (int, optional): The maximum number of times a worker's 
-    computation will be attempted, in the event of failure. Defaults to 2.
-    
-    timeout (None, optional): If a computation runs for longer than this 
-    value, it will be cancelled. By default, there is no limit.
-    
-    dependencies (None, optional): Other tasks that this task depends on. 
-    By default, this task is assumed to have no dependencies.
-    
-    *args: Additional arguments to pass to the worker computation.
-    **kwargs: Additional keyword arguments to pass to the worker computation.
-
-  Returns:
-    list: A list of the results returned by each worker's computation.
-  """
-  
-  # create the task
-  task = BlockwiseTask(
-    worker_file,
-    context,
-    total_box,
-    read_box,
-    write_box,
-    num_workers,
-    max_attempts,
-    timeout,
-    dependencies,
+    worker_file: str | Path,
+    compute_context: ComputeContext | str,
+    total_roi: Roi,
+    read_roi: Roi,
+    write_roi: Roi,
+    num_workers: int = 16,
+    max_retries: int = 2,
+    timeout=None,
+    upstream_tasks=None,
     *args,
     **kwargs,
-  )
-  
-  # run the task with Daisy
-  return daisy.run_blockwise([task])
+):
+    """Run a function in parallel over a large volume.
+
+    Args:
+
+        worker_file (``str`` or ``Path``):
+
+            The path to the file containing the necessary worker functions:
+            ``spawn_worker`` and ``start_worker``.
+            Optionally, the file can also contain a ``check_function`` and an ``init_callback_fn``.
+
+        total_roi (``Roi``):
+            The ROI to process.
+
+        read_roi (``Roi``):
+            The ROI to read from for a block.
+
+        write_roi (``Roi``):
+            The ROI to write to for a block.
+
+        num_workers (``int``):
+
+                The number of workers to use.
+
+        max_retries (``int``):
+
+                    The maximum number of times a task will be retried if failed
+                    (either due to failed post check or application crashes or network
+                    failure)
+
+        compute_context (``ComputeContext``):
+
+            The compute context to use for parallelization.
+
+        *args:
+
+            Additional positional arguments to pass to ``worker_function``.
+
+        **kwargs:
+
+            Additional keyword arguments to pass to ``worker_function``.
+
+    Returns:
+
+            ``Bool``.
+
+    """
+
+    # Make the task
+    task = DaCapoBlockwiseTask(
+        worker_file,
+        compute_context,
+        total_roi,
+        read_roi,
+        write_roi,
+        num_workers,
+        max_retries,
+        timeout,
+        upstream_tasks,
+        *args,
+        **kwargs,
+    )
+
+    return daisy.run_blockwise([task])
+
+
+def segment_blockwise(
+    segment_function_file: str or Path,
+    compute_context: ComputeContext | str,
+    context: Coordinate,
+    total_roi: Roi,
+    read_roi: Roi,
+    write_roi: Roi,
+    num_workers: int = 16,
+    max_retries: int = 2,
+    timeout=None,
+    upstream_tasks=None,
+    tmp_prefix="tmp",
+    *args,
+    **kwargs,
+):
+    with tempfile.TemporaryDirectory(prefix=tmp_prefix) as tmpdir:
+        # write parameters to tmpdir
+        if "parameters" in locals():
+            with open(Path(tmpdir, "parameters.yaml"), "w") as f:
+                yaml.dump(locals()["parameters"], f)
+
+        # Make the task
+        task = DaCapoBlockwiseTask(
+            str(Path(Path(__file__).parent, "segment_worker.py")),
+            compute_context,
+            total_roi.grow(context, context),
+            read_roi,
+            write_roi,
+            num_workers,
+            max_retries,
+            timeout,
+            upstream_tasks,
+            tmpdir=tmpdir,
+            function_path=segment_function_file,
+            *args,
+            **kwargs,
+        )
+
+        daisy.run_blockwise([task])
+
+        # give a second for the fist task to finish
+        time.sleep(1)
+        read_roi = write_roi
+
+        success = daisy.run_blockwise([task])
+
+        # Make the task
+        task = DaCapoBlockwiseTask(
+            str(Path(Path(__file__).parent, "relabel_worker.py")),
+            compute_context,
+            total_roi,
+            read_roi,
+            write_roi,
+            num_workers,
+            max_retries,
+            timeout,
+            upstream_tasks,
+            tmpdir=tmpdir,
+            *args,
+            **kwargs,
+        )
+
+        return success and daisy.run_blockwise([task])
--- dacapo/blockwise/threshold_worker.py
+++ dacapo/blockwise/threshold_worker.py
@@ -1,31 +1,114 @@
-"""
-This script sets up a worker for the Dacapo Python library to perform data processing tasks. It performs these tasks 
-using the ZarrArray class and LocalArrayIdentifier class. 
+from pathlib import Path
+from dacapo.experiments.datasplits.datasets.arrays.zarr_array import ZarrArray
+from dacapo.store.array_store import LocalArrayIdentifier
+from dacapo.compute_context import ComputeContext, LocalTorch
 
-There are two main interfaces provided:
-1. start_worker command: This gets arguments from the command line and then performs certain tasks such as getting arrays,
-   waiting for blocks to run pipeline, and writing to output array.
-2. spawn_worker function: This function is responsible for creating and running the worker in the given compute context.
-   It sets up a command line for running the worker and then executes it with the selected compute context.
+import daisy
 
-The script uses Daiy's Client instance to interact with the workers and manages the lifecycle of these workers.
+import numpy as np
+import click
 
-Functions:
-cli(log_level) -> None:
-    This function sets up the command line interface of script with various options and
-    sets the logging level of the interface.
+import logging
 
-start_worker(input_container: Path | str,input_dataset: str,output_container: Path | str,
-             output_dataset: str,threshold: float = 0.0); -> None:
-    This function grabs arrays, waits for blocks to run pipeline, and writes to an output array. It gets the necessary
-    parameters from the command line options.
+logger = logging.getLogger(__file__)
 
-spawn_worker(input_array_identifier: "LocalArrayIdentifier", output_array_identifier: "LocalArrayIdentifier", 
-             threshold: float = 0.0,compute_context: ComputeContext = LocalTorch()); -> Callable:
-    This function creates and runs the worker in the given compute context.
-    It sets up a command line for running the worker, and then executes it with the selected compute context. The function 
-    returns the worker function.
+read_write_conflict: bool = False
+fit: str = "valid"
 
-__name__ == "__main__" -> None:
-    This is the entry point of the script. It calls the command line interface function.
-"""
+
+@click.group()
+@click.option(
+    "--log-level",
+    type=click.Choice(
+        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False
+    ),
+    default="INFO",
+)
+def cli(log_level):
+    logging.basicConfig(level=getattr(logging, log_level.upper()))
+
+
+@cli.command()
+@click.option(
+    "-ic",
+    "--input_container",
+    required=True,
+    type=click.Path(exists=True, file_okay=False),
+)
+@click.option("-id", "--input_dataset", required=True, type=str)
+@click.option(
+    "-oc", "--output_container", required=True, type=click.Path(file_okay=False)
+)
+@click.option("-od", "--output_dataset", required=True, type=str)
+@click.option("-th", "--threshold", type=float, default=0.0)
+def start_worker(
+    input_container: Path | str,
+    input_dataset: str,
+    output_container: Path | str,
+    output_dataset: str,
+    threshold: float = 0.0,
+):
+    # get arrays
+    input_array_identifier = LocalArrayIdentifier(Path(input_container), input_dataset)
+    input_array = ZarrArray.open_from_array_identifier(input_array_identifier)
+
+    output_array_identifier = LocalArrayIdentifier(
+        Path(output_container), output_dataset
+    )
+    output_array = ZarrArray.open_from_array_identifier(output_array_identifier)
+
+    # wait for blocks to run pipeline
+    client = daisy.Client()
+
+    while True:
+        print("getting block")
+        with client.acquire_block() as block:
+            if block is None:
+                break
+
+            # write to output array
+            output_array[block.write_roi] = (
+                input_array[block.write_roi] > threshold
+            ).astype(np.uint8)
+
+
+def spawn_worker(
+    input_array_identifier: "LocalArrayIdentifier",
+    output_array_identifier: "LocalArrayIdentifier",
+    threshold: float = 0.0,
+    compute_context: ComputeContext = LocalTorch(),
+):
+    """Spawn a worker to predict on a given dataset.
+
+    Args:
+        model (Model): The model to use for prediction.
+        raw_array (Array): The raw data to predict on.
+        prediction_array_identifier (LocalArrayIdentifier): The identifier of the prediction array.
+        compute_context (ComputeContext, optional): The compute context to use. Defaults to LocalTorch().
+    """
+    # Make the command for the worker to run
+    command = [
+        "python",
+        __file__,
+        "start-worker",
+        "--input_container",
+        input_array_identifier.container,
+        "--input_dataset",
+        input_array_identifier.dataset,
+        "--output_container",
+        output_array_identifier.container,
+        "--output_dataset",
+        output_array_identifier.dataset,
+        "--threshold",
+        threshold,
+    ]
+
+    def run_worker():
+        # Run the worker in the given compute context
+        compute_context.execute(command)
+
+    return run_worker
+
+
+if __name__ == "__main__":
+    cli()
