3f5ee5d2c5d599d94e7dc65eca1bb4a0375fb43e
David Ackerman <ackermand@janelia.hhmi.org>
2024-03-13 10:56:18 -0400

    fix: :bug: fix yaml load error by using full_load instead of safe_load

--- dacapo/store/file_config_store.py
+++ dacapo/store/file_config_store.py
@@ -103,7 +103,7 @@ class FileConfigStore(ConfigStore):
 
         else:
             with file_store.open("r") as f:
-                existing = yaml.safe_load(f)
+                existing = yaml.full_load(f)
 
             if not self.__same_doc(existing, data, ignore):
                 raise DuplicateNameError(
@@ -116,7 +116,7 @@ class FileConfigStore(ConfigStore):
         file_store = collection / f"{name}.yaml"
         if file_store.exists():
             with file_store.open("r") as f:
-                return yaml.safe_load(f)
+                return yaml.full_load(f)
         else:
             raise ValueError(f"No config with name: {name} in collection: {collection}")
 
