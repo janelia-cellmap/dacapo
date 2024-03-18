bda506736401bb39a603e921c41737ca352a9324
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2024-03-18 17:37:23 -0400

    fix serialization

--- dacapo/experiments/starts/cosem_start_config.py
+++ dacapo/experiments/starts/cosem_start_config.py
@@ -1,14 +1,12 @@
 import attr
 from .cosem_start import CosemStart
-
+from .start_config import StartConfig
 
 @attr.s
-class CosemStartConfig:
+class CosemStartConfig(StartConfig):
     """Starter for COSEM pretained models. This is a subclass of `StartConfig` and
     should be used to initialize the model with pretrained weights from a previous
     run.
     """
 
     start_type = CosemStart
-
-    name: str = attr.ib(metadata={"help_text": "The COSEM checkpoint name to use."})
