6bb5d47a9d064b6d95c6607de5f4d6e4252d454b
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2024-03-19 16:51:16 -0400

    head matching

--- dacapo/experiments/starts/start.py
+++ dacapo/experiments/starts/start.py
@@ -3,6 +3,19 @@ import logging
 
 logger = logging.getLogger(__file__)
 
+head_keys = ["prediction_head.weight","prediction_head.bias","chain.1.weight","chain.1.bias"]
+
+def match_heads(model, head_weights, old_head, new_head ):
+    for label in new_head:
+        if label in old_head:
+            logger.warning(f"matching head for {label}.")
+            old_index = old_head.index(label)
+            new_index = new_head.index(label)
+            for key in head_keys:
+                if key in model.state_dict().keys():
+                    n_val = head_weights[key][old_index]
+                    model.state_dict()[key][new_index] = n_val
+            logger.warning(f"matched head for {label}.")
 
 class Start(ABC):
     """
@@ -32,28 +45,55 @@ class Start(ABC):
         self.run = start_config.run
         self.criterion = start_config.criterion
 
-    def _set_weights(self, model, weights):
-        print(f"loading weights from run {self.run}, criterion: {self.criterion}")
-        # load the model weights (taken from torch load_state_dict source)
-        try:
-            model.load_state_dict(weights.model)
-        except RuntimeError as e:
-            logger.warning(e)
-            # if the model is not the same, we can try to load the weights
-            # of the common layers
-            model_dict = model.state_dict()
-            pretrained_dict = {
-                k: v
-                for k, v in weights.model.items()
-                if k in model_dict and v.size() == model_dict[k].size()
-            }
-            model_dict.update(
-                pretrained_dict
-            )  # update only the existing and matching layers
-            model.load_state_dict(model_dict)
-            logger.warning(f"loaded only common layers from weights")
+        if hasattr(start_config.task_config,"channels"):
+            self.channels = start_config.task_config.channels
+        else:
+            self.channels = None
 
-    def initialize_weights(self, model):
+    def _set_weights(self, model, weights,new_head=None):
+        print(f"loading weights from run {self.run}, criterion: {self.criterion}")
+        try:
+            if self.channels and new_head:
+                try:
+                    logger.warning(f"matching heads from run {self.run}, criterion: {self.criterion}")
+                    logger.warning(f"old head: {self.channels}")
+                    logger.warning(f"new head: {new_head}")
+                    head_weights = {}
+                    for key in head_keys:
+                        head_weights[key] = weights.model[key]
+                    for key in head_keys:
+                        weights.model.pop(key, None)
+                    model.load_state_dict(weights.model, strict=False)
+                    model = match_heads(model, head_weights, self.channels, new_head)
+                except RuntimeError as e:
+                    logger.error(f"ERROR starter matching head: {e}")
+                    logger.warning(f"removing head from run {self.run}, criterion: {self.criterion}")
+                    for key in head_keys:
+                        weights.model.pop(key, None)
+                    model.load_state_dict(weights.model, strict=False)
+                    logger.warning(f"loaded weights in non strict mode from run {self.run}, criterion: {self.criterion}")
+            else:
+                try:
+                    model.load_state_dict(weights.model)
+                except RuntimeError as e:
+                    logger.warning(e)
+                    model_dict = model.state_dict()
+                    pretrained_dict = {
+                        k: v
+                        for k, v in weights.model.items()
+                        if k in model_dict and v.size() == model_dict[k].size()
+                    }
+                    model_dict.update(
+                        pretrained_dict
+                    ) 
+                    model.load_state_dict(model_dict)
+                    logger.warning(f"loaded only common layers from weights")
+        except RuntimeError as e:
+            logger.warning(f"ERROR starter: {e}")
+
+        
+
+    def initialize_weights(self, model,new_head=None):
         """
         Retrieves the weights from the dacapo store and load them into
         the model.
@@ -72,4 +112,5 @@ class Start(ABC):
 
         weights_store = create_weights_store()
         weights = weights_store._retrieve_weights(self.run, self.criterion)
-        self._set_weights(model, weights)
+        self._set_weights(model, weights,new_head)
+
