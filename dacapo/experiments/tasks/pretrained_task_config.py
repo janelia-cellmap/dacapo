f3a739a3cf5169009001a80c60bb2c5dfafd1e5b
avweigel <weigela@janelia.hhmi.org>
2024-03-28 16:44:44 -0400

    docs: :memo: this commit will add documentation to pretrained_task_config.py

--- dacapo/experiments/tasks/pretrained_task_config.py
+++ dacapo/experiments/tasks/pretrained_task_config.py
@@ -8,7 +8,18 @@ from pathlib import Path
 
 @attr.s
 class PretrainedTaskConfig(TaskConfig):
-    """ """
+    """
+    Configuration for a task that uses a pretrained model. The model is loaded from a file
+    and the weights are loaded from a file.
+    
+    Attributes:
+        sub_task_config (TaskConfig): The task to run starting with the provided pretrained weights.
+        weights (Path): A checkpoint containing pretrained model weights.
+    Methods:
+        verify(self) -> Tuple[bool, str]: This method verifies the PretrainedTaskConfig object.
+    Notes:
+        This is a subclass of TaskConfig.
+    """
 
     task_type = PretrainedTask
 
