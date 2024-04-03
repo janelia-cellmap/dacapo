9bac56e4534d5cfd9b1e69713fe80e9481ac198a
avweigel <weigela@janelia.hhmi.org>
2024-04-03 16:46:52 -0400

    docs: :memo: add documentation to merge_instance_array_config.py

--- dacapo/experiments/datasplits/datasets/arrays/merge_instances_array_config.py
+++ dacapo/experiments/datasplits/datasets/arrays/merge_instances_array_config.py
@@ -8,6 +8,20 @@ from typing import List
 
 @attr.s
 class MergeInstancesArrayConfig(ArrayConfig):
+    """
+    Configuration for an array that merges instances from multiple arrays
+    into a single array. The instances are merged by taking the union of the
+    instances in the source arrays.
+    
+    Attributes:
+        source_array_configs: List[ArrayConfig]
+            The Array of masks from which to take the union
+    Methods:
+        create_array: () -> MergeInstancesArray
+            Create a MergeInstancesArray instance from the configuration
+    Notes:
+        The MergeInstancesArrayConfig class is used to create a MergeInstancesArray
+    """
     array_type = MergeInstancesArray
 
     source_array_configs: List[ArrayConfig] = attr.ib(
