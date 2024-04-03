3b62f72894908b1dc2eefa8411d0c2ceb3fed83f
avweigel <weigela@janelia.hhmi.org>
2024-04-03 07:27:24 -0400

    docs: :memo: add documentation to resampled_array_config.py

--- dacapo/experiments/datasplits/datasets/arrays/resampled_array_config.py
+++ dacapo/experiments/datasplits/datasets/arrays/resampled_array_config.py
@@ -8,7 +8,20 @@ from funlib.geometry import Coordinate
 
 @attr.s
 class ResampledArrayConfig(ArrayConfig):
-    """This array will up or down sample an array into the desired voxel size."""
+    """
+    A configuration for a ResampledArray. This array will up or down sample an array into the desired voxel size. 
+
+    Attributes:
+        source_array_config (ArrayConfig): The Array that you want to upsample or downsample.
+        upsample (Coordinate): The amount by which to upsample!
+        downsample (Coordinate): The amount by which to downsample!
+        interp_order (bool): The order of the interpolation!
+    Methods:
+        create_array: Creates a ResampledArray from the configuration.
+    Note:
+        This class is meant to be used with the ArrayDataset class.
+    
+    """
 
     array_type = ResampledArray
 
