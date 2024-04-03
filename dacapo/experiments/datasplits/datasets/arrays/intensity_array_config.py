6d937feee338eeeb3cb2e1d04acbcbcb3c18ca27
avweigel <weigela@janelia.hhmi.org>
2024-04-03 17:13:32 -0400

    docs: :memo: add documentation to intensity_array_config.py

--- dacapo/experiments/datasplits/datasets/arrays/intensity_array_config.py
+++ dacapo/experiments/datasplits/datasets/arrays/intensity_array_config.py
@@ -6,8 +6,20 @@ from .intensity_array import IntensitiesArray
 
 @attr.s
 class IntensitiesArrayConfig(ArrayConfig):
-    """This config class provides the necessary configuration for turning an Annotated dataset into a
-    multi class binary classification problem"""
+    """
+    This config class provides the necessary configuration for turning an Annotated dataset into a
+    multi class binary classification problem. It takes a source array and normalizes the intensities
+    between 0 and 1. The source array is expected to contain a volume with uint64 voxels and no channel dimension.
+        
+    Attributes:
+        source_array_config (ArrayConfig): The Array from which to pull annotated data
+        min (float): The minimum intensity in your data
+        max (float): The maximum intensity in your data
+    Methods:
+        to_array: Returns the IntensitiesArray object
+    Notes:
+        The source_array_config must be an ArrayConfig object.
+    """
 
     array_type = IntensitiesArray
 
