6cee2765b7cf734018273d8cbb2081da18da729f
avweigel <weigela@janelia.hhmi.org>
2024-04-04 07:06:57 -0400

    docs: :memo: add documentation to array_config.py

--- dacapo/experiments/datasplits/datasets/arrays/array_config.py
+++ dacapo/experiments/datasplits/datasets/arrays/array_config.py
@@ -5,9 +5,22 @@ from typing import Tuple
 
 @attr.s
 class ArrayConfig:
-    """Base class for array configurations. Each subclass of an
+    """
+    Base class for array configurations. Each subclass of an
     `Array` should have a corresponding config class derived from
-    `ArrayConfig`.
+    `ArrayConfig`. This class should be used to store the configuration
+    of the array.
+
+    Attributes:
+        name (str): A unique name for this array. This will be saved so you
+            and others can find and reuse this array. Keep it short
+            and avoid special characters.
+    Methods:
+        verify(self) -> Tuple[bool, str]: This method is used to check whether this is a valid Array.
+    Note:
+        This class is used to create a base class for array configurations. Each subclass of an
+        `Array` should have a corresponding config class derived from `ArrayConfig`.
+        This class should be used to store the configuration of the array.
     """
 
     name: str = attr.ib(
@@ -21,5 +34,18 @@ class ArrayConfig:
     def verify(self) -> Tuple[bool, str]:
         """
         Check whether this is a valid Array
+
+        Returns:
+            Tuple[bool, str]: A tuple with the first element being a boolean
+            indicating whether the array is valid and the second element being
+            a string with a message explaining why the array is invalid
+        Raises:
+            NotImplementedError: This method is not implemented in this class
+        Examples:
+            >>> array_config = ArrayConfig(name="array_config")
+            >>> array_config.verify()
+            (True, "No validation for this Array")
+        Note:
+            This method is used to check whether this is a valid Array.
         """
         return True, "No validation for this Array"
