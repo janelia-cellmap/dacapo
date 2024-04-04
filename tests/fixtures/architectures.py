dc62c66a0e1d187bc3c9c338e4b2bb481d73fb69
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2024-04-04 16:20:58 -0400

    upgrade tests to  pytest8

--- tests/fixtures/architectures.py
+++ tests/fixtures/architectures.py
@@ -1,4 +1,4 @@
-from dacapo.experiments.architectures import DummyArchitectureConfig
+from dacapo.experiments.architectures import DummyArchitectureConfig, CellposUNetConfig
 
 import pytest
 
@@ -7,4 +7,12 @@ import pytest
 def dummy_architecture():
     yield DummyArchitectureConfig(
         name="dummy_architecture", num_in_channels=1, num_out_channels=12
+    )
+
+@pytest.fixture
+def cellpose_architecture():
+    yield CellposUNetConfig(
+        name="cellpose_architecture", input_shape=(216, 216, 216),
+        nbase=[1, 12, 24, 48, 96], nout = 12, conv_3D = True
+        # nbase=[1, 32, 64, 128, 256], nout = 32, conv_3D = True
     )
