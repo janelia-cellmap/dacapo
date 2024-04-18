75369bdb87b7f03b7bbd01c5b882416092ced827
rhoadesScholar <rhoadesj@hhmi.org>
2024-04-18 13:50:59 -0400

    chore: 🙈 Make path_to_worker in dummy_script obvious

--- examples/blockwise/dummy_script.py
+++ examples/blockwise/dummy_script.py
@@ -2,13 +2,14 @@ from dacapo.blockwise.scheduler import run_blockwise
 from funlib.geometry import Roi
 
 # Make the ROIs
+path_to_worker = "dummy_worker.py"
 total_roi = Roi(offset=(0, 0, 0), shape=(100, 100, 100))
 read_roi = Roi(offset=(0, 0, 0), shape=(10, 10, 10))
 write_roi = Roi(offset=(0, 0, 0), shape=(1, 1, 1))
 
 # Run the script blockwise
 success = run_blockwise(
-    worker_file="dummy_worker.py",
+    worker_file=path_to_worker,
     total_roi=total_roi,
     read_roi=read_roi,
     write_roi=write_roi,
