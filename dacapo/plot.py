7f534cfe22debd002185b97acbff49260ffd0931
mzouink <mzouink@users.noreply.github.com>
2024-07-31 20:42:31 +0000

    :art: Format Python code with psf/black

--- dacapo/plot.py
+++ dacapo/plot.py
@@ -17,8 +17,7 @@ import matplotlib.pyplot as plt
 
 import os
 
-    
-        
+
 RunInfo = namedtuple(
     "RunInfo",
     [
@@ -448,14 +447,22 @@ def plot_runs(
                 criteria=run.validation_score_name
             )
             colors_val = itertools.cycle(plt.cm.tab20.colors)
-            for dataset,color_v in zip(run.validation_scores.datasets,colors_val):
+            for dataset, color_v in zip(run.validation_scores.datasets, colors_val):
                 dataset_data = validation_score_data.sel(datasets=dataset)
                 include_validation_figure = True
                 x = [score.iteration for score in run.validation_scores.scores]
                 cc = next(colors_val)
                 for i in range(dataset_data.data.shape[1]):
-                    current_name = f"{i}_{dataset.name}_{name}_{run.validation_score_name}"
-                    validation_ax.plot(x, dataset_data.data[:,i] , label=current_name, color=cc, alpha=0.5+0.2*i)
+                    current_name = (
+                        f"{i}_{dataset.name}_{name}_{run.validation_score_name}"
+                    )
+                    validation_ax.plot(
+                        x,
+                        dataset_data.data[:, i],
+                        label=current_name,
+                        color=cc,
+                        alpha=0.5 + 0.2 * i,
+                    )
         print("VALIDATION PLOTTED")
 
     if include_loss_figure:
