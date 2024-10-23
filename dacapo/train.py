7b1821019f2ec47ecf61e38c010ae9f9b62a2f68
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2024-10-23 17:49:24 +0200

    tqdm all in one line

--- dacapo/train.py
+++ dacapo/train.py
@@ -146,23 +146,26 @@ def train_run(run: Run, do_validate=True):
     )
 
     with run.trainer as trainer:
+        bar = None
         while trained_until < run.train_until:
             # train for at most 100 iterations at a time, then store training stats
             iterations = min(100, run.train_until - trained_until)
             iteration_stats = None
-            bar = tqdm(
-                trainer.iterate(
-                    iterations,
-                    run.model,
-                    run.optimizer,
-                    compute_context.device,
-                ),
-                desc=f"training until {iterations + trained_until}",
-                total=run.train_until,
-                initial=trained_until,
-            )
-            for iteration_stats in bar:
+            if bar is None:
+                bar = tqdm(
+                    total=run.train_until,
+                    initial=trained_until,
+                    desc=f"training until {run.train_until}",
+                )
+
+            for iteration_stats in trainer.iterate(
+                iterations,
+                run.model,
+                run.optimizer,
+                compute_context.device,
+            ):
                 run.training_stats.add_iteration_stats(iteration_stats)
+                bar.update(1)
                 bar.set_postfix({"loss": iteration_stats.loss})
 
                 if (iteration_stats.iteration + 1) % run.validation_interval == 0:
