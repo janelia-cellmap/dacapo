0147983d0a974e8e86258259e83564d5303b0215
William Patton <wllmpttn24@gmail.com>
2025-01-02 09:59:58 -0800

    add wrapped architecture to architectures `__init__.py`

--- dacapo/experiments/architectures/__init__.py
+++ dacapo/experiments/architectures/__init__.py
@@ -3,3 +3,4 @@ from .dummy_architecture_config import (
     DummyArchitectureConfig,
 )  # noqa
 from .cnnectome_unet import CNNectomeUNetConfig  # noqa
+from .wrapped_architecture import WrappedArchitectureConfig  # noqa
