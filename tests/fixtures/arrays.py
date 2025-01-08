c3ef15375fbeb38badacc20b631cc75db69be8f4
Yurii Zubov <zubov452@gmail.com>
2025-01-08 11:04:13 -0500

    Add multiscale dataset fixture with ome-ngff metadata

--- tests/fixtures/arrays.py
+++ tests/fixtures/arrays.py
@@ -6,7 +6,7 @@ from dacapo.experiments.datasplits.datasets.arrays import (
 
 import zarr
 import numpy as np
-
+from numcodecs import Zstd
 import pytest
 
 
@@ -59,3 +59,60 @@ def cellmap_array(tmp_path):
     )
 
     yield cellmap_array_config
+    
+@pytest.fixture(scope="session")
+def metadata_ome_zarr():
+    zarr_metadata = {
+        "multiscales": [
+            {
+                "axes": [
+                    {"name": "z", "type": "space", "unit": "nanometer"},
+                    {"name": "y", "type": "space", "unit": "nanometer"},
+                    {"name": "x", "type": "space", "unit": "nanometer"},
+                ],
+                "coordinateTransformations": [],
+                "datasets": [
+                        {
+                        "coordinateTransformations": [
+                            {"scale": [4.2,7.4,5.6], "type": "scale"},
+                            {"translation": [6.0,10.0,2.0],"type": "translation"}
+                        ],
+                        "path": "s0"
+                        },
+                        {
+                        "coordinateTransformations": [
+                            {"type": "scale","scale": [8.4,14.8,11.2]},
+                            {"type": "translation","translation": [8.1,13.7,4.8]}
+                        ],
+                        "path": "s1"
+                        }
+                ],
+                "name": "multiscale_dataset",
+                "version": "0.4",
+            }
+        ]
+    }
+    return zarr_metadata
+
+    
+@pytest.fixture(scope="session")
+def multiscale_dataset(tmp_path, metadata_ome_zarr):
+    path = tmp_path.mktemp("test", numbered=False)
+    test_zarr_path = path / "multiscale_zarr_dataset.zarr"
+    
+    store = zarr.DirectoryStore(test_zarr_path)
+    multiscale_group = zarr.group(store=store, path="multiscale_dataset", overwrite=True)
+
+    for level in [0,1]:
+        scaling = pow(2, level)
+        multiscale_group.require_dataset(
+            name=f's{level}',
+            shape=(100/scaling, 80/scaling, 60/scaling),
+            chunks=10,
+            dtype=np.float32,
+            compressor=Zstd(level=6),
+        )
+
+    multiscale_group.attrs.update(metadata_ome_zarr)
+
+    yield multiscale_group
