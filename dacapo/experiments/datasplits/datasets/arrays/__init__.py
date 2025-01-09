bab7ff52f075f2bd22bba5f6249ec232f5db3cca
mzouink <zouinkhi.marwan@gmail.com>
2025-01-09 14:50:57 -0500

    remove fibsem:

--- dacapo/experiments/datasplits/datasets/arrays/__init__.py
+++ dacapo/experiments/datasplits/datasets/arrays/__init__.py
@@ -6,7 +6,6 @@ from .zarr_array_config import ZarrArrayConfig  # noqa
 from .binarize_array_config import BinarizeArrayConfig  # noqa
 from .resampled_array_config import ResampledArrayConfig  # noqa
 from .intensity_array_config import IntensitiesArrayConfig  # noqa
-from .missing_annotations_mask_config import MissingAnnotationsMaskConfig  # noqa
 from .ones_array_config import OnesArrayConfig  # noqa
 from .concat_array_config import ConcatArrayConfig  # noqa
 from .logical_or_array_config import LogicalOrArrayConfig  # noqa
