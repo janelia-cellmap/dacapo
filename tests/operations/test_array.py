1bcdc775ef4c0d8cc8b4e1fda809539be36bc372
mzouink <mzouink@users.noreply.github.com>
2025-01-19 18:35:48 +0000

    :art: Format Python code with psf/black

--- tests/operations/test_array.py
+++ tests/operations/test_array.py
@@ -19,10 +19,21 @@ logging.basicConfig(level=logging.INFO)
 def test_array(array_config):
     array = array_config.array()
 
-    assert array.offset == (12, 12, 12), f"offset is not correct, expected (12, 12, 12), got {array.offset}"
-    assert array.voxel_size == (1, 2, 4), f"resolution is not correct, expected (1, 2, 4), got {array.voxel_size}"
-    assert array.axis_names == ["z", "y", "x"], f"axis names are not correct, expected ['z', 'y', 'x'], got {array.axis_names}"
+    assert array.offset == (
+        12,
+        12,
+        12,
+    ), f"offset is not correct, expected (12, 12, 12), got {array.offset}"
+    assert array.voxel_size == (
+        1,
+        2,
+        4,
+    ), f"resolution is not correct, expected (1, 2, 4), got {array.voxel_size}"
+    assert array.axis_names == [
+        "z",
+        "y",
+        "x",
+    ], f"axis names are not correct, expected ['z', 'y', 'x'], got {array.axis_names}"
 
     # offset = array.attrs["offset"]
     # resolution = array.attrs["resolution"]
-
